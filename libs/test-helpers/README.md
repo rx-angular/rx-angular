0296ace0de6c93c27d178f003479d5184d8cd787
Michael Hladky <michael@hladky.at>
2020-04-02 14:01:17 +0200

    test(common) implement test helpers

---
+++ libs/test-helpers/README.md
@@ -0,0 +1,7 @@
+# test-helpers
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `ng test test-helpers` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/test-helpers/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'test-helpers',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/libs/test-helpers'
+};
