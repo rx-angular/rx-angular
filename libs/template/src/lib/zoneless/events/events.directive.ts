3e26e4e29f7e169a9721830f441d30b15720f257
micha <michael@hladky.at>
2020-05-08 23:56:53 +0200

    docs(template): zone-less events directive add event list

--- libs/template/src/lib/zoneless/events/events.directive.ts
+++ libs/template/src/lib/zoneless/events/events.directive.ts
@@ -3,16 +3,44 @@ import { BehaviorSubject, Subscription } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { revertEventListenerZonePatch } from '../../core/utils/make-zone-less';
 
+const zonePatchedEvents = [
+  'scroll',
+  'mousedown',
+  'mouseenter',
+  'mouseleave',
+  'mousemove',
+  'mouseout',
+  'mouseover',
+  'mouseup',
+  'load',
+  'pointerup',
+  'change',
+  'blur',
+  'focus',
+  'click',
+  'contextmenu',
+  'drag',
+  'dragend',
+  'dragenter',
+  'dragleave',
+  'dragover',
+  'dragstart',
+  'drop',
+  'input'
+];
+
 // tslint:disable-next-line:directive-selector
 @Directive({ selector: '[zoneless]' })
 export class EventsDirective implements OnDestroy {
   subscription = new Subscription();
-  events$ = new BehaviorSubject<string[]>(['click']);
+  events$ = new BehaviorSubject<string[]>(zonePatchedEvents);
 
   @Input('zoneless')
   set events(value: string[]) {
     if (value && value.length > 0) {
       this.events$.next(value);
+    } else {
+      this.events$.next(zonePatchedEvents);
     }
   }
 
