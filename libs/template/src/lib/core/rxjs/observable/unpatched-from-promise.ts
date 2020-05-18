b66312df2de6039fe5fb5bd4c9fe6eaea0b34a0a
micha <michael@hladky.at>
2020-05-18 23:32:34 +0200

    docs(template): progress demo

--- libs/template/src/lib/core/rxjs/observable/unpatched-from-promise.ts
+++
@@ -1,7 +0,0 @@
-import { defer, from, Observable } from 'rxjs';
-import { mapTo } from 'rxjs/operators';
-import { getUnpatchedResolvedPromise } from '../../utils/unpatched-promise';
-
-export function tickFromUnPatchedPromise(): Observable<number> {
-  return defer(() => from(getUnpatchedResolvedPromise()).pipe(mapTo(1)));
-}
