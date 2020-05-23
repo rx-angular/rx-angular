a9bf1815859265cd839662779152ea5862f3af02
micha <michael@hladky.at>
2020-05-23 13:47:35 +0200

    test(general): test coverage report cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)', '!(**/spec/**/fixtures.*)'],
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   transform: {
     '^.+\\.(ts|js|html)$': 'ts-jest'
   },
