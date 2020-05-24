fdb6bb3bd48692147b40d40546d6e709ffedc498
Michael Hladky <michael@hladky.at>
2020-05-24 02:03:12 +0200

    test(state): adopted test

--- libs/state/spec/core/utils/pipe-from-array.spec.ts
+++ libs/state/spec/core/utils/pipe-from-array.spec.ts
@@ -12,10 +12,10 @@ import { map } from 'rxjs/operators';
 describe('pipeFromArray', () => {
 
   it('should return true for arrays of function', () => {
-    expect(pipeFromArray(undefined as any)).toBeTruthy();
-    expect(pipeFromArray([])).toBeTruthy();
-    expect(pipeFromArray([map(() => {})])).toBeTruthy();
-    expect(pipeFromArray([map(() => {}), () => {}])).toBeTruthy();
+    expect(typeof pipeFromArray(undefined as any) === 'function').toBeTruthy();
+    expect(typeof pipeFromArray([]) === 'function').toBeTruthy();
+    expect(typeof pipeFromArray([map(() => {})]) === 'function').toBeTruthy();
+    expect(typeof pipeFromArray([map(() => {}), () => {}]) === 'function').toBeTruthy();
   });
 
 });
