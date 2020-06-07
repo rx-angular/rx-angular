c96156b9560557ecbbb51a4a3dfacd40ad81d8c7
Michael Hladky <michael@hladky.at>
2020-06-07 16:07:25 +0200

    fix(state): exports

--- libs/state/src/lib/index.ts
+++ libs/state/src/lib/index.ts
@@ -1,3 +1,8 @@
 export { RxState } from './rx-state.service';
-export { select, stateful, distinctUntilSomeChanged } from './rxjs/operators';
+export {
+  select,
+  stateful,
+  distinctUntilSomeChanged,
+  selectSlice,
+} from './rxjs/operators';
 export { KeyCompareMap, CompareFn } from './rxjs/interfaces';
