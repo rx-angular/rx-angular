744147f4e3b6610c779258ac3edc160640edbb62
Michael Hladky <michael@hladky.at>
2020-06-11 14:18:22 +0200

    feat(state): add perf target (#134)
    Added perf checks as nx target for state

---
+++ libs/state/perf/core/transformation-helpers/one-of.suite.ts
@@ -0,0 +1,50 @@
+// tslint:disable
+import { BenchmarkSuite } from '../../utils';
+
+export const oneOfSuite: BenchmarkSuite = {
+  testSet: {
+    'Array#includes': arrayIncludes,
+    'Object#prop': objectProperty,
+    expression: expression,
+    'Array#includes2': arrayIncludes2,
+    'Object#prop2': objectProperty2,
+    expression2: expression2
+  }
+};
+
+function arrayIncludes() {
+  ['string', 'symbol', 'number'].includes(typeof 'test');
+}
+
+function objectProperty() {
+  const obj: any = {
+    string: true,
+    symbol: true,
+    number: true
+  };
+  obj[typeof 'test'];
+}
+
+function expression() {
+  typeof 'test' === 'string' ||
+    typeof 'test' === 'symbol' ||
+    typeof 'test' === 'number';
+}
+const arr = ['string', 'symbol', 'number'];
+function arrayIncludes2() {
+  arr.includes(typeof 'test');
+}
+
+const obj: any = {
+  string: true,
+  symbol: true,
+  number: true
+};
+function objectProperty2() {
+  obj[typeof 'test'];
+}
+
+const typeOf = typeof 'test';
+function expression2() {
+  typeOf === 'string' || typeOf === 'symbol' || typeOf === 'number';
+}
