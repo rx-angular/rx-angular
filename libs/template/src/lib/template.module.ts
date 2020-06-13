607575348fc981455773e16f3029f9415445ab07
Michael Hladky <michael@hladky.at>
2020-06-13 15:54:21 +0200

    feat(template) add unpatch events module and directive (#137)
    * feat(template) add unpatch events module and directive
    
    * add examples

--- libs/template/src/lib/template.module.ts
+++ libs/template/src/lib/template.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { LetModule } from './let';
 import { PushModule } from './push';
+import { UnpatchEventsModule } from './unpatch/events';
 
 @NgModule({
-  exports: [LetModule, PushModule]
+  exports: [LetModule, PushModule, UnpatchEventsModule]
 })
 export class TemplateModule {}
