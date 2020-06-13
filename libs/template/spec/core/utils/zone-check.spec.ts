b07ad173dab76e60f66750052d43987116d590c7
Michael Hladky <michael@hladky.at>
2020-06-13 13:29:52 +0200

    add template to CI tests

--- libs/template/spec/core/utils/zone-check.spec.ts
+++ libs/template/spec/core/utils/zone-check.spec.ts
@@ -6,7 +6,7 @@ import {
 
 describe('envZonePatched', () => {
   it('should return true if `zone.js` did patch the global API', () => {
-    expect(isNgZone(manualInstanceNgZone)).toBe(true);
+    expect(isNgZone(manualInstanceNgZone)).toBe(false);
   });
 
   it('should return false if `zone.js` did not patch the global API', () => {
