cf0398710f50762b566b4724e2b8296d0c16dfa4
Kirill <karnaukhov.kh@gmail.com>
2020-06-21 22:13:41 +0300

    Update libs/state/src/lib/transformation-helpers/object/mergeObjects.ts
    Co-authored-by: Michael Hladky <michael@hladky.at>

--- libs/state/src/lib/transformation-helpers/object/mergeObjects.ts
+++ libs/state/src/lib/transformation-helpers/object/mergeObjects.ts
@@ -15,7 +15,7 @@ import { isObjectGuard } from '../../core/utils/typing';
  *
  * const catWithname = mergeObjects(cat, {name: 'Fluffy'});
  *
- * // anonymusCat will be:
+ * // anonymousCat will be:
  * // {id: 1, type: 'cat', name: 'Fluffy'};
  *
  * @returns T
