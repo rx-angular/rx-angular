a1565d4b9356fc7b3d488ca36ec45b33679629d0
kuba_dospial <j.dospial@dodax.com>
2020-06-26 19:51:49 +0200

    fixed object-diver

--- libs/state/src/lib/core/utils/object-diver.ts
+++ libs/state/src/lib/core/utils/object-diver.ts
@@ -1,11 +1,13 @@
+import { isKeyOf } from './typing';
+
 export function objectDiver<T>(
   obj: T | Partial<T>,
   keys: string[]
 ): T | Partial<T> {
   const key = keys.shift();
-  const value = obj[key];
-  if (!!key) {
-    return !!value && keys.length > 0 ? objectDiver(value, keys) : value;
+  const value = isKeyOf(key) ? obj[key] : null;
+  if (!!value) {
+    return keys.length > 0 ? objectDiver(value, keys) : value;
   }
   return obj;
 }
