67184982a67c616f68024d3ae359076795454e85
Michael Hladky <michael@hladky.at>
2020-07-05 15:06:55 +0200

    test(template) improve tests

--- libs/template/jest.config.js
+++ libs/template/jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
   name: 'template',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/template',
-  collectCoverageFrom: [
-    "./src/**/*.ts"
-  ],
+  collectCoverageFrom: ['./src/**/*.ts'],
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
