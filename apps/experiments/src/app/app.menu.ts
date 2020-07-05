7d233c6c826a758271001e9b2a55fc65be071c2b
Julian <julian@jandl.wien>
2020-07-05 19:14:04 +0200

    fix(experiments): fixed broken imports

--- apps/experiments/src/app/app.menu.ts
+++ apps/experiments/src/app/app.menu.ts
@@ -4,13 +4,12 @@ import { MENU_ITEMS as CD_MENU_ITEMS } from './cd/cd.menu';
 import { MENU_ITEMS as PUSH_MENU_ITEMS } from './push/push.menu';
 import { MENU_ITEMS as LET_MENU_ITEMS } from './let/let.menu';
 import { MENU_ITEMS as MIXED_MENU_ITEMS } from './mixed/mixed.menu';
-import { MENU_ITEMS as DRAFT_MENU_ITEMS } from './draft/draft.menu';
+// import { MENU_ITEMS as DRAFT_MENU_ITEMS } from './draft/draft.menu';
 import { MENU_ITEMS as CD_OPERATORS_MENU_ITEMS } from './cd-operators/cd-operators.menu';
 import { MENU_ITEMS as RX_STATE_MENU_ITEMS } from './state/rx-state.menu';
 import { STRATEGIES_MENU } from './strategies/strategies.menu';
 
 export const MENU_ITEMS: MenuItem[] = [
-  ...DRAFT_MENU_ITEMS,
   ...CD_MENU_ITEMS,
   ...PUSH_MENU_ITEMS,
   ...LET_MENU_ITEMS,
