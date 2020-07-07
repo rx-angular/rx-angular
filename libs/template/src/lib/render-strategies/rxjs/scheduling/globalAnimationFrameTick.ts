ff89471f86525dae38643a3411d05bd23fb62ff8
Michael Hladky <michael@hladky.at>
2020-07-07 19:20:46 +0200

    added comments to discuss

--- libs/template/src/lib/render-strategies/rxjs/scheduling/globalAnimationFrameTick.ts
+++ libs/template/src/lib/render-strategies/rxjs/scheduling/globalAnimationFrameTick.ts
@@ -1,14 +1,16 @@
 import { MonoTypeOperatorFunction, Observable } from 'rxjs';
 import { map, switchMap, switchMapTo } from 'rxjs/operators';
-import { globalTaskManager, GlobalTask } from '../../../core/render-aware';
+import { GlobalTask, globalTaskManager } from '../../../core/render-aware';
 
 export function scheduleOnGlobalTick<T>(
   workDefinitionFn: () => GlobalTask
 ): MonoTypeOperatorFunction<T> {
+  // Local queue of references of the work function needed to dispose their execution
   const workToDeplete = [];
   const depleteQueue$ = new Observable<void>(subscriber => {
     subscriber.next();
     return () => {
+      // Exhaust local queue
       while (workToDeplete.length > 0) {
         const w = workToDeplete.pop();
         globalTaskManager.deleteTask(w);
@@ -16,6 +18,16 @@ export function scheduleOnGlobalTick<T>(
     };
   });
   return (o$: Observable<T>) => {
+    // To clarify
+
+    // If we could switchMap into the next schedule call
+    // switchMap(() => globalWorker.scheduleTask(scheduledTask))
+    // pro: do less work in global queue
+    // cons: end up in eternal scheduling
+
+    // Is it important to tie the signal of an executed work to the related scheduled work,
+    // meaning maintaining the order of emission?
+
     return depleteQueue$.pipe(
       switchMapTo(o$),
       switchMap(val => {
