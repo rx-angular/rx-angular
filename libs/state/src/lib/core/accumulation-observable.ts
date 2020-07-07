e962096b48ccb7ec775b48ffe66cfa4b3f29ec68
kirill-karnaukhov <kirill.karnaukhov@vilmate.com>
2020-07-07 21:29:47 +0300

    Log error in tap

--- libs/state/src/lib/core/accumulation-observable.ts
+++ libs/state/src/lib/core/accumulation-observable.ts
@@ -52,7 +52,10 @@ export function createAccumulationObservable<T extends object>(
       (state, [slice, stateAccumulator]) => stateAccumulator(state, slice),
       {} as T
     ),
-    tap(newState => (compositionObservable.state = newState)),
+    tap(
+      newState => (compositionObservable.state = newState),
+      error => console.error(error)
+    ),
     publish()
   );
   const state$: Observable<T> = signal$.pipe(publishReplay(1));
@@ -91,11 +94,6 @@ export function createAccumulationObservable<T extends object>(
     sub.add(
       (compositionObservable.state$ as ConnectableObservable<T>).connect()
     );
-    sub.add(() => {
-      accumulatorObservable.complete();
-      stateObservables.complete();
-      stateSlices.complete();
-    });
     return sub;
   }
 }
