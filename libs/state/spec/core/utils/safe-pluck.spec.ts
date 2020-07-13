80e0c57601c8db62afa726fc52914d1c5bf3a8d1
Michael Hladky <michael@hladky.at>
2020-07-13 17:02:32 +0200

    Update libs/state/spec/core/utils/safe-pluck.spec.ts

--- libs/state/spec/core/utils/safe-pluck.spec.ts
+++ libs/state/spec/core/utils/safe-pluck.spec.ts
@@ -3,7 +3,10 @@ import { safePluck } from '../../../src/lib/core/utils';
 describe('safePluck', () => {
     const bar = 'hello'
     const obj = { foo: { bar } };
-
+    it('should not touch passed arguments', () => {
+        const args = ['foo', 'bar'];
+        expect(safePluck(args.length)).toBe(2);
+    })
     it('should return `bar` when keys are provided', () => {
         expect(safePluck(obj, ['foo', 'bar'])).toEqual(bar);
     })
