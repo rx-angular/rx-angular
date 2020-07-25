82b71a248e16ab0f35819f1cd23a296a3b17f115
Michael Hladky <michael@hladky.at>
2020-07-25 21:08:20 +0200

    ng update @nrwl/angular  @nrwl/jest ng update @nrwl/angular  @nrwl/jest

--- apps/state-demo/jest.config.js
+++ apps/state-demo/jest.config.js
@@ -5,6 +5,17 @@ module.exports = {
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js'
-  ]
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
 };
--- libs/state/jest.config.js
+++ libs/state/jest.config.js
@@ -2,12 +2,21 @@ module.exports = {
   name: '@rx-angular/state',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/state',
-  collectCoverageFrom: [
-    "./src/**/*.ts"
-  ],
+  collectCoverageFrom: ['./src/**/*.ts'],
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js'
-  ]
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
 };
