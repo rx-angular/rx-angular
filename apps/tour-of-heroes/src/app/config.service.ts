a8bcea5baa491b627a8f5ab3cbd125fce5041351
Michael Hladky <michael@hladky.at>
2020-08-23 21:38:48 +0200

    docs(tour-of-heroes): implemented render count

--- apps/tour-of-heroes/src/app/config.service.ts
+++ apps/tour-of-heroes/src/app/config.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { RxState } from '@rx-angular/state';
+import { getStrategies } from '@rx-angular/template';
 
 export enum AppRenderStrategy {
   noop = 'noop',
   native = 'native',
   local = 'local',
-  global = 'global',
+  global = 'global'
 }
 
 export interface AppConfig {
@@ -13,17 +14,21 @@ export interface AppConfig {
 }
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ConfigService {
   // doing this in order to NOT expose the full state API. We don't have to unsubscribe in a global service either.
   private readonly state = new RxState<AppConfig>();
 
   readonly renderStrategy$ = this.state.select('renderStrategy');
-
+  strategies = Object.keys(getStrategies({ cdRef: { context: {} } } as any));
   constructor() {}
 
   setStrategy(renderStrategy: AppRenderStrategy) {
     this.state.set({ renderStrategy });
+  }
+
+  setStrategyByName(renderStrategyName: string) {
+    this.state.set({ renderStrategy: this.strategies[renderStrategyName] });
   }
 }
