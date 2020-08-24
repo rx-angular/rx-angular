6a4741683a2375752e1bb0ffbd42b2977e6306d9
Michael Hladky <michael@hladky.at>
2020-08-24 13:46:37 +0200

    fix(export-experimental): export interfaces for custom strategies

--- libs/template/src/index.ts
+++ libs/template/src/index.ts
@@ -1,9 +1,6 @@
-export {
-  getStrategies,
-  priorityTickMap,
-  SchedulingPriority,
-} from './lib/render-strategies';
 // EXPERIMENTAL
+export { RenderStrategy, RenderStrategyFactoryConfig } from './lib/core/render-aware';
+export {coalesceWith } from './lib/render-strategies/rxjs/operators/coalesceWith';
 export {
   UnpatchEventsDirective,
   UnpatchEventsModule,
@@ -15,6 +12,13 @@ export {
 // @TODO clarify if we should exports this
 export { getZoneUnPatchedApi, isNgZone } from './lib/core';
 export { isViewEngineIvy } from './lib/experimental/core/utils/view-engine-checks';
+
+// STABLE
+export {
+  getStrategies,
+  priorityTickMap,
+  SchedulingPriority,
+} from './lib/render-strategies';
 
 export { PushPipe, PushModule } from './lib/push';
 export { LetDirective, LetModule } from './lib/let';
