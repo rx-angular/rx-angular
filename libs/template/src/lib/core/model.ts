3120fe06b4491b6a8541a1325b85f4b9b5c7c3dd
Michael Hladky <michael@hladky.at>
2020-09-05 19:57:25 +0200

    fix(rx-view-context): set error ro occurred one

--- libs/template/src/lib/core/model.ts
+++ libs/template/src/lib/core/model.ts
@@ -4,7 +4,7 @@ export interface RxViewContext<T> {
   // to enable `let` syntax we have to use $implicit (var; let v = var)
   $implicit: T;
   // set context var complete to true (var$; let e = $error)
-  $error: boolean;
+  $error: false | Error;
   // set context var complete to true (var$; let c = $complete)
   $complete: boolean;
   // set context var suspense to true (var$; let c = $suspense)
