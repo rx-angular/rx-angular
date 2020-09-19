8900c672d7bbf982cb33bc6e40d865c64f4758c6
Julian <julian@jandl.wien>
2020-09-20 00:34:00 +0200

    feat(render-aware): added missing files

---
+++ apps/experiments/src/app/render-callback/render-callback-overview.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rx-render-callback-overview',
+  template: `
+
+  `,
+  styles: [`
+
+
+  `],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RenderCallbackOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ apps/experiments/src/app/render-callback/render-callback.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TemplateModule } from '@rx-angular/template';
+import { RendersModule } from '../renders/renders.module';
+import { RenderCallback01Component } from './01/render-callback-01.component';
+import { RenderCallbackOverviewComponent } from './render-callback-overview.component';
+import { RENDER_CALLBACK_ROUTES } from './render-callback.routes';
+
+@NgModule({
+  declarations: [RenderCallbackOverviewComponent, RenderCallback01Component],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(RENDER_CALLBACK_ROUTES),
+    TemplateModule,
+    RendersModule
+  ]
+})
+export class RenderCallbackModule { }
