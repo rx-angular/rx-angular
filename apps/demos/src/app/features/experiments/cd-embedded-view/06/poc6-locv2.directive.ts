d8e5acbb92197fc33626b58ca94dde47e6402dda
Michael Hladky <michael@hladky.at>
2020-10-06 02:26:00 +0200

    wip

--- apps/demos/src/app/features/experiments/cd-embedded-view/06/poc6-locv2.directive.ts
+++ apps/demos/src/app/features/experiments/cd-embedded-view/06/poc6-locv2.directive.ts
@@ -67,7 +67,6 @@ export class Poc6Locv2Directive<U> implements OnInit, OnDestroy {
   ngOnInit() {
     this.subscription = this.values$
       .pipe(
-        tap(),
         mergeMap(arr => arr),
         groupBy(i => i[this.poc6LocV2TrackBy]),
         map(o$ => {
--- apps/demos/src/app/features/experiments/cd-embedded-view/rx-iterable-differ/parent.component.ts
+++ apps/demos/src/app/features/experiments/cd-embedded-view/rx-iterable-differ/parent.component.ts
@@ -1,6 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { environment } from '../../../../../environments/environment';
 import { ArrayProviderService } from '../../../../shared/debug-helper/value-provider';
+import { Observable, Subject } from 'rxjs';
+import { distinctUntilChanged, groupBy, map, mergeAll, mergeMap, tap } from 'rxjs/operators';
+import { toDictionary } from '../../../../../../../../libs/state/src/lib/transformation-helpers/array';
 
 
 @Component({
@@ -52,5 +55,52 @@ export class CdEmbeddedViewParentRxDifferComponent {
 
   constructor(public valP: ArrayProviderService) {
 
+  }
+}
+
+function rxIterableDiffer<T extends object>(config: { trackByKey: keyof T, distinctByKey: keyof T }): {
+  next: (v: T[]) => void,
+  enter$: Observable<T>,
+  update$: Observable<T>,
+  exit$: Observable<T>
+} {
+  const array$ = new Subject<T[]>();
+  let idMap = {};
+  const item$$ = array$.pipe(
+    tap(a => {
+      // @TODO good idea?? I guess no... :D
+      idMap = toDictionary(a, config.trackByKey as any)
+    }),
+    mergeMap(arr => arr),
+    groupBy(i => i[config.trackByKey])
+  );
+  const enter$ = item$$.pipe(
+    map(o$ => o$
+      .pipe(distinctUntilChanged((a, b) => a[config.distinctByKey] === b[config.distinctByKey]))
+    ),
+    mergeAll()
+  );
+  const update$ = item$$.pipe(
+    map(o$ => o$
+      .pipe(distinctUntilChanged((a, b) => a[config.distinctByKey] === b[config.distinctByKey]))
+    ),
+    mergeAll()
+  );
+  const exit$ = item$$.pipe(
+    map(o$ => o$
+      .pipe(distinctUntilChanged((a, b) => a[config.distinctByKey] === b[config.distinctByKey]))
+    ),
+    mergeAll()
+  );
+
+  return {
+    next,
+    enter$,
+    update$,
+    exit$
+  };
+
+  function next(v: T[]): void {
+    array$.next(v);
   }
 }
