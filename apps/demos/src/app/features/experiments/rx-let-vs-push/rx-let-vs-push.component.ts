a069a0196a94e6047c569390696827326d0a678d
Kirill Karnaukhov <karnaukhov.kh@gmail.com>
2020-10-06 22:16:55 +0300

    Fix markup

--- apps/demos/src/app/features/experiments/rx-let-vs-push/rx-let-vs-push.component.ts
+++ apps/demos/src/app/features/experiments/rx-let-vs-push/rx-let-vs-push.component.ts
@@ -5,49 +5,62 @@ import { environment } from '../../../../environments/environment';
 @Component({
   selector: 'rxa-rx-let-vs-push',
   template: `
-    <h1>RxLet vs Push</h1>
-    <div style="margin-bottom: 16px">
-      <button mat-raised-button color="primary" (click)="togglePushAutoTest()">
-        Run Auto test for Push pipe (Start performance profiler beforehand)
-      </button>
-    </div>
-    <div style="margin-bottom: 16px">
-      <button mat-raised-button color="secondary" (click)="togglePush()">
-        Open Manual test for Push pipe
-      </button>
-    </div>
-    <div style="margin-bottom: 16px">
-      <button mat-raised-button color="primary" (click)="toggleLetAutoTest()">
-        Run Auto test for Let directive (Start performance profiler beforehand)
-      </button>
-    </div>
-    <div style="margin-bottom: 16px">
-      <button mat-raised-button color="secondary" (click)="toggleLet()">
-        Open Manual test for Let directive
-      </button>
-    </div>
-
+    <h1>Push vs RxLet</h1>
     <div class="row w-100">
-      <rxa-list-toggle-test-component
-        *ngIf="showPushAutoTest"
-        type="push"
-        [auto]="true"
-      ></rxa-list-toggle-test-component>
-      <rxa-list-toggle-test-component
-        *ngIf="showPush"
-        type="push"
-        [auto]="false"
-      ></rxa-list-toggle-test-component>
-      <rxa-list-toggle-test-component
-        *ngIf="showLetAutoTest"
-        type="let"
-        [auto]="true"
-      ></rxa-list-toggle-test-component>
-      <rxa-list-toggle-test-component
-        *ngIf="showLet"
-        type="let"
-        [auto]="false"
-      ></rxa-list-toggle-test-component>
+      <div class="col-sm-12 col-md-6">
+        <div style="margin-bottom: 16px">
+          <button
+            mat-raised-button
+            color="primary"
+            (click)="togglePushAutoTest()"
+          >
+            Run Auto test for Push pipe (Start performance profiler beforehand)
+          </button>
+        </div>
+        <div style="margin-bottom: 16px">
+          <button mat-raised-button color="secondary" (click)="togglePush()">
+            Open Manual test for Push pipe
+          </button>
+        </div>
+        <rxa-list-toggle-test-component
+          *ngIf="showPushAutoTest"
+          type="push"
+          [auto]="true"
+        ></rxa-list-toggle-test-component>
+        <rxa-list-toggle-test-component
+          *ngIf="showPush"
+          type="push"
+          [auto]="false"
+        ></rxa-list-toggle-test-component>
+      </div>
+
+      <div class="col-sm-12 col-md-6">
+        <div style="margin-bottom: 16px">
+          <button
+            mat-raised-button
+            color="primary"
+            (click)="toggleLetAutoTest()"
+          >
+            Run Auto test for Let directive (Start performance profiler
+            beforehand)
+          </button>
+        </div>
+        <div style="margin-bottom: 16px">
+          <button mat-raised-button color="secondary" (click)="toggleLet()">
+            Open Manual test for Let directive
+          </button>
+        </div>
+        <rxa-list-toggle-test-component
+          *ngIf="showLetAutoTest"
+          type="let"
+          [auto]="true"
+        ></rxa-list-toggle-test-component>
+        <rxa-list-toggle-test-component
+          *ngIf="showLet"
+          type="let"
+          [auto]="false"
+        ></rxa-list-toggle-test-component>
+      </div>
     </div>
   `,
   changeDetection: environment.changeDetection,
