fd4d3676ef6498ed3dd4cd0b4db7af49327b60ab
Kirill Karnaukhov <karnaukhov.kh@gmail.com>
2020-10-06 23:01:33 +0300

    Fix let vs push route and remove it from experimental section

--- apps/demos/src/app/features/experiments/rx-let-vs-push/rx-let-vs-push.menu.ts
+++ apps/demos/src/app/features/experiments/rx-let-vs-push/rx-let-vs-push.menu.ts
@@ -3,10 +3,9 @@ export const RXLET_VS_PUSH_MENU_ITEMS = [
     link: 'rx-let-vs-push',
     label: 'RxLet vs Push',
     children: [
-      // 01.
       {
-        link: 'rx-let-vs-push',
-        label: 'RxLet vs Push',
+        link: 'list-toggle',
+        label: 'List toggle',
       },
     ],
   },
