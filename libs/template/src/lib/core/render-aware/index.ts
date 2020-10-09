9cefcf1dcf61113a7ab41cc0ea0e46f895c2054c
Julian <julian@jandl.wien>
2020-10-09 02:30:14 +0200

    fix(let): fixed broken export

--- libs/template/src/lib/core/render-aware/index.ts
+++ libs/template/src/lib/core/render-aware/index.ts
@@ -1,7 +1,6 @@
 export * from './render-aware_creator';
 export * from './interfaces';
 export { RxNotification } from '../model';
-export { NotificationExtract } from '../model';
 export { RxNotificationKind } from '../model';
 export { notificationKindToRxNotificationKind } from '../utils/rx-materialize';
 export { rxMaterialize } from '../utils/rx-materialize';
