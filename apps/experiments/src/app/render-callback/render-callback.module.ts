9d84af2680a918faa1b69b3a846d4b6f96351a1b
Julian <julian@jandl.wien>
2020-10-09 02:35:17 +0200

    fix(demo): fixed render callback demo

--- apps/experiments/src/app/render-callback/render-callback.module.ts
+++ apps/experiments/src/app/render-callback/render-callback.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RouterModule } from '@angular/router';
 import { TemplateModule } from '@rx-angular/template';
-import { RendersModule } from '../renders/renders.module';
 import { RenderCallback01Component } from './01/render-callback-01.component';
 import { RenderCallback02Component } from './02/render-callback-02.component';
 import { RenderCallback03Component } from './03/render-callback-03.component';
@@ -25,7 +24,6 @@ import { RENDER_CALLBACK_ROUTES } from './render-callback.routes';
     CommonModule,
     RouterModule.forChild(RENDER_CALLBACK_ROUTES),
     TemplateModule,
-    RendersModule,
     MatProgressSpinnerModule
   ]
 })
