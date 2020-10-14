5cf2e80733801c95cb681a227bdd6fcd11e84f68
kajetan.swiatek <kajetan.swiatek@aptitudesoftware.com>
2020-10-14 09:02:08 +0200

    chore(performance benchmarks): added first POC for integrating Chrome Devtool Protocol with Cypress tests

--- apps/demos-e2e/src/integration/app.spec.ts
+++ apps/demos-e2e/src/integration/app.spec.ts
@@ -1,13 +1,126 @@
 import { getGreeting } from '../support/app.po';
 
 describe('demos', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.task('resetCRI').visit('/experiments/rx-let-vs-push/list-toggle'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  xdescribe('test with Performance', () => {
+    it('should check performance of push pipe on 1st try', () => {
+      cy.get('button').contains('Open Manual test for Push').click();
+      cy.task('enablePerformanceMetrics');
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0').task('getPerformanceMetrics');
+      cy.task('disablePerformanceMetrics');
+    });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to demos!');
+    it('should check performance of push pipe on 2nd try', () => {
+      cy.get('button').contains('Open Manual test for Push').click();
+
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0');
+      cy.get('button').contains('Toggle').click();
+      cy.wait(500);
+
+      cy.task('enablePerformanceMetrics');
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0').task('getPerformanceMetrics');
+      cy.task('disablePerformanceMetrics');
+    });
+
+    it('should check performance of let directive on 1st try', () => {
+      cy.get('button').contains('Open Manual test for Let').click();
+
+      cy.task('enablePerformanceMetrics');
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0').task('getPerformanceMetrics');
+      cy.task('disablePerformanceMetrics');
+    });
+
+    it('should check performance of let directive on 2nd try', () => {
+      cy.get('button').contains('Open Manual test for Let').click();
+
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0');
+      cy.get('button').contains('Toggle').click();
+      cy.wait(500);
+
+      cy.task('enablePerformanceMetrics');
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0').task('getPerformanceMetrics');
+      cy.task('disablePerformanceMetrics');
+    });
+  });
+
+  xdescribe('test with Profiler', () => {
+    it('should check performance of push pipe', () => {
+      cy.get('button').contains('Open Manual test for Push').click();
+
+      cy.task('enableProfiler');
+      cy.task('startProfiler');
+
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0');
+      cy.get('button').contains('Toggle').click();
+      cy.wait(2000);
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0');
+
+      cy.task('stopProfiler');
+      cy.task('disableProfiler');
+    });
+
+    it('should check performance of let directive', () => {
+      cy.get('button').contains('Open Manual test for Let').click();
+
+      cy.task('enableProfiler');
+      cy.task('startProfiler');
+
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0');
+      cy.get('button').contains('Toggle').click();
+      cy.wait(2000);
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0');
+
+      cy.task('stopProfiler');
+      cy.task('disableProfiler');
+    });
+  });
+
+  describe('test with Tracing', () => {
+    it('should check performance of push pipe', () => {
+      cy.get('button').contains('Open Manual test for Push').click();
+
+      cy.task('startTracing');
+
+      openAndCloseToggle();
+      openAndCloseToggle();
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0');
+      cy.wait(2000);
+
+      cy.task('endTracing');
+    });
+
+    xit('should check performance of let directive', () => {
+      cy.get('button').contains('Open Manual test for Let').click();
+
+      cy.task('startTracing');
+
+      openAndCloseToggle();
+      openAndCloseToggle();
+      cy.get('button').contains('Toggle').click();
+      cy.get('rxa-rendering-work').contains('0');
+      cy.wait(2000);
+
+      cy.task('endTracing');
+    });
   });
 });
+
+function openAndCloseToggle() {
+  cy.get('button').contains('Toggle').click();
+  cy.get('rxa-rendering-work').contains('0');
+  cy.wait(2000);
+  cy.get('button').contains('Toggle').click();
+  cy.wait(2000);
+}
--- apps/demos-e2e/src/plugins/index.js
+++ apps/demos-e2e/src/plugins/index.js
@@ -1,17 +1,13 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
 const { preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor');
+const CDP = require('chrome-remote-interface');
+const debug = require('debug')('cypress:server:protocol');
+const fs = require('fs');
+
+let port = 0;
+let client = null;
+let tracingEvents = [];
+const TRACE_CATEGORIES = ["-*", "devtools.timeline", "disabled-by-default-devtools.timeline", "disabled-by-default-devtools.timeline.frame", "toplevel", "blink.console", "disabled-by-default-devtools.timeline.stack", "disabled-by-default-devtools.screenshot", "disabled-by-default-v8.cpu_profile", "disabled-by-default-v8.cpu_profiler", "disabled-by-default-v8.cpu_profiler.hires"];
+
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
@@ -19,4 +15,128 @@ module.exports = (on, config) => {
 
   // Preprocess Typescript file using Nx helper
   on('file:preprocessor', preprocessTypescript(config));
+
+  on('before:browser:launch', (browser, launchOptionsOrArgs) => {
+    debug('browser launch args or options %o', launchOptionsOrArgs);
+    const args = Array.isArray(launchOptionsOrArgs)
+      ? launchOptionsOrArgs
+      : launchOptionsOrArgs.args;
+
+    port = ensureRdpPort(args);
+    debug('ensureRdpPort %d', port);
+    debug('Chrome arguments %o', args);
+  });
+
+  on('task', {
+    resetCRI: async () => {
+      if (client) {
+        debug('resetting CRI client');
+        await client.close();
+        client = null;
+      }
+
+      return Promise.resolve(true);
+    },
+
+    getPerformanceMetrics: async () => {
+      debug('getPerformanceMetrics');
+      client = client || await CDP({ port });
+
+      return client.Performance.getMetrics();
+    },
+
+    enablePerformanceMetrics: async () => {
+      debug('enablePerformanceMetrics');
+      client = client || await CDP({ port });
+
+      return client.Performance.enable();
+    },
+
+    disablePerformanceMetrics: async () => {
+      debug('disablePerformanceMetrics');
+      client = client || await CDP({ port });
+
+      return client.Performance.disable();
+    },
+
+    enableProfiler: async () => {
+      debug('enableProfiler');
+      client = client || await CDP({ port });
+
+      const { Profiler } = client;
+      await Profiler.setSamplingInterval({ interval: 100 });
+
+      return Profiler.enable();
+    },
+
+    disableProfiler: async () => {
+      debug('disableProfiler');
+      client = client || await CDP({ port });
+
+      return client.Profiler.disable();
+    },
+
+    startProfiler: async () => {
+      debug('startProfiler');
+      client = client || await CDP({ port });
+
+      return client.Profiler.start();
+    },
+
+    stopProfiler: async () => {
+      debug('stopProfiler');
+      client = client || await CDP({ port });
+
+      const profilerResults = await client.Profiler.stop();
+
+      fs.writeFileSync('./profiler-results.cpuprofile', JSON.stringify(profilerResults.profile || 'Profile not found'));
+
+      return Promise.resolve(profilerResults);
+    },
+
+    startTracing: async () => {
+      debug('startTracing');
+      client = client || await CDP({ port });
+      const { Page, Tracing } = client;
+
+      await Page.enable();
+      tracingEvents = [];
+      Tracing.dataCollected(({ value }) => {
+        tracingEvents.push(...value);
+      });
+
+      return Tracing.start({
+        categories: TRACE_CATEGORIES.join(','),
+        options: 'sampling-frequency=10000'
+      });
+    },
+
+    endTracing: async () => {
+      debug('endTracing');
+      client = client || await CDP({ port });
+
+      await client.Tracing.end();
+      const tracingComplete = await client.Tracing.tracingComplete();
+
+      fs.writeFileSync('./tracing-results.json', JSON.stringify(tracingEvents));
+
+      return Promise.resolve(tracingComplete);
+    }
+  });
 };
+
+function ensureRdpPort(args) {
+  const existing = args.find(
+    (arg) => arg.slice(0, 23) === '--remote-debugging-port'
+  );
+
+  if (existing) {
+    return Number(existing.split('=')[1]);
+  }
+
+  const port = 40000 + Math.round(Math.random() * 25000);
+
+  args.push(`--remote-debugging-port=${port}`);
+
+  return port;
+}
