4d6a69d24d10fe93631b9369dc7315685a89ab73
Chau Tran <nartc7789@gmail.com>
2020-11-10 11:38:09 -0600

    docs: add pokemon w/ pagination demo

---
+++ apps/demos/src/app/features/integrations/pokemon-pagination/states/pokemon-state.service.ts
@@ -0,0 +1,117 @@
+import { Injectable } from '@angular/core';
+import { RxState } from '@rx-angular/state';
+import { combineLatest } from 'rxjs';
+import {
+  distinctUntilChanged,
+  map,
+  switchMap,
+  withLatestFrom,
+} from 'rxjs/operators';
+import { Pokemon } from '../models/pokemon.model';
+import { PokemonService } from '../services/pokemon.service';
+
+export interface PokemonState {
+  status: 'loading' | 'success';
+  total: number;
+  originalResult: Pokemon[];
+  filteredResult: Pokemon[];
+  limit: number;
+  offset: number;
+  currentPage: number;
+  query: string;
+}
+
+@Injectable()
+export class PokemonStateService extends RxState<PokemonState> {
+  readonly originalResult$ = this.select('originalResult');
+  readonly total$ = this.select('total');
+  readonly limit$ = this.select('limit');
+  readonly offset$ = this.select('offset');
+
+  /**
+   * Construct the ViewModel that the Presentation Layer will use
+   */
+  readonly vm$ = combineLatest([
+    this.select('status'),
+    this.select('filteredResult'),
+    this.select('currentPage'),
+    this.total$,
+    this.limit$,
+    this.offset$,
+  ]).pipe(
+    map(([status, filteredResult, currentPage, total, limit, offset]) => ({
+      status,
+      filteredResult,
+      total,
+      currentPage,
+      limit,
+      offset,
+    }))
+  );
+
+  constructor(private readonly pokemonService: PokemonService) {
+    super();
+    /**
+     * Set the initial value for these.
+     */
+    this.set({
+      currentPage: 1,
+      limit: 20,
+      offset: 0,
+      query: '',
+      filteredResult: [],
+      originalResult: [],
+      total: 0,
+    });
+
+    /**
+     * Connect the main effect$ (fetching data based on limit$ and offset$ changed) to the State
+     */
+    this.connect(this.effect$());
+
+    /**
+     * Connect the queryEffect$ (client-side text filter) to filteredResult
+     */
+    this.connect('filteredResult', this.queryEffect$());
+  }
+
+  private effect$() {
+    return combineLatest([this.limit$, this.offset$]) // combine limit$ and offset$ which effectively watches for changes on these two
+      .pipe(
+        withLatestFrom(this.total$, this.originalResult$), // grab the latest values of total$ and originalResult$
+        switchMap(
+          (
+            [[limit, offset], total, original] // switch to getPokemon to fetch the pokemon with the new limit and offset
+          ) =>
+            this.pokemonService.getPokemon(limit, offset, {
+              count: total,
+              next: '',
+              previous: '',
+              results: original,
+            }) // total and original helps to construct the prev data
+        ),
+        map(({ status, data }) => ({
+          // transform data returned by service to the shape of our State
+          status,
+          total: data.count,
+          filteredResult: data.results,
+          originalResult: data.results,
+        }))
+      );
+  }
+
+  private queryEffect$() {
+    return this.select('query') // watch for query changed
+      .pipe(
+        distinctUntilChanged(), // rate limiting
+        withLatestFrom(this.originalResult$), // grab latest value from originalResult$
+        map(([query, data]) => (!query ? data : this.filter(data, query))) // check condition and call filter if needed
+      );
+  }
+
+  private filter(data: Pokemon[], query: string): Pokemon[] {
+    return data.filter((res) =>
+      res.name.toLowerCase().includes(query.toLowerCase())
+    );
+  }
+}
