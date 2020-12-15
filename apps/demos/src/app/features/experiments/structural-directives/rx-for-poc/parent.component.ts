d0c3b0d47c2df8e54fe77bdb255d71adfa268897
Julian <julian@jandl.wien>
2020-12-15 21:43:43 +0100

    feat(rx-for): fixed minimal rxfor setup again

--- apps/demos/src/app/features/experiments/structural-directives/rx-for-poc/parent.component.ts
+++ apps/demos/src/app/features/experiments/structural-directives/rx-for-poc/parent.component.ts
@@ -45,6 +45,9 @@ import { immutableArr } from '../utils';
                                    [value]="displayStates.all"
                                    #group="matButtonToggleGroup">
             <mat-button-toggle [value]="displayStates.native">Native</mat-button-toggle>
+            <mat-button-toggle [value]="displayStates.rxAngularMinimalReactive">RxAngular *rxMinimalFor trackBy,
+              distinctBy, select
+            </mat-button-toggle>
             <mat-button-toggle [value]="displayStates.rxAngularReactive">RxAngular *rxFor trackBy, distinctBy, select
             </mat-button-toggle>
             <mat-button-toggle [value]="displayStates.all">All</mat-button-toggle>
@@ -73,6 +76,28 @@ import { immutableArr } from '../utils';
 
             <ng-container *ngFor="let i of value.arr; trackBy: trackById">
               <rxa-rx-for-value [value]="i"></rxa-rx-for-value>
+            </ng-container>
+          </rxa-visualizer>
+        </div>
+        <div class="col"
+             *ngIf="group.value === displayStates.rxAngularMinimalReactive || group.value === displayStates.all">
+          <h2>RxAngular, *rxMinimalFor trackBy, distinctBy, select</h2>
+          <p>
+            <button mat-raised-button [unpatch] (click)="changeOneClick$.next(1)">
+              unpatched update
+            </button>
+            <button mat-raised-button [unpatch] (click)="changeAllClick$.next(10)">
+              unpatched Change all
+            </button>
+            <button mat-raised-button [unpatch] (click)="toggleIntervalClick$.next(10)">
+              unpatched toggel interval
+            </button>
+            <rxa-strategy-select (strategyChange)="strategy$.next($event)"></rxa-strategy-select>
+          </p>
+          <rxa-visualizer viewType="embedded-view" *rxMinimalFor="array$; let i; let r$ = record$; let select = select">
+            <span #spanChild></span>
+            <ng-container *rxMinimalFor="select(['arr']); trackBy: tK; distinctBy:dK; let v$ = record$;">
+              <rxa-rx-for-value [strategy$]="strategy$" [value]="v$"></rxa-rx-for-value>
             </ng-container>
           </rxa-visualizer>
         </div>
@@ -124,7 +149,8 @@ export class RxForContainerComponent extends RxState<{ rows: number, columns: nu
     native: 0,
     nativeReactive: 1,
     rxAngularReactive: 2,
-    all: 3
+    rxAngularMinimalReactive: 3,
+    all: 4
   };
 
   childrenRendered = new Subject<void>();
