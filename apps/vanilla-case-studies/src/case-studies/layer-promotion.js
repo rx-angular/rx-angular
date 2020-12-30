e5261ce6d76fb82dd5d619f37cfa3fed52b52d5f
Michael Hladky <michael@hladky.at>
2020-12-30 07:41:33 +0100

    add test fn

--- apps/vanilla-case-studies/src/case-studies/layer-promotion.js
+++ apps/vanilla-case-studies/src/case-studies/layer-promotion.js
@@ -6,9 +6,19 @@ function test(num= 1, opts = {}) {
   }
 }
 
+function styleStress(num= 1, opts = {styles: {background: 'red'}}) {
+  for(let i = 0; i < num; i++) {
+    document.body.appendChild(getChild(opts));
+  }
+}
+
+
 function getChild(opts = {}) {
   const div = document.createElement('DIV');
   div.innerHTML = opts?.class || '&nbsp;';
   opts?.class && div.classList.add(opts.class);
+  opts?.styles && Object.entries(opts.styles).forEach(([rule, value]) => {
+    div.style[rule] = value;
+  });
   return div;
 }
