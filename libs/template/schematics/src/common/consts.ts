bd1e12d984338d03a5edd4875fd5f9b8f81ba5f1
Michael Hladky <michael@hladky.at>
2021-02-22 23:45:07 +0100

    release(template): adopt schematics

--- libs/template/schematics/src/common/consts.ts
+++ libs/template/schematics/src/common/consts.ts
@@ -1,1 +1,12 @@
+import {
+  NodeDependency,
+  NodeDependencyType,
+} from '@schematics/angular/utility/dependencies';
+
 export const packageName = '@rx-angular/template';
+export const peerDependencies: NodeDependency = {
+  type: NodeDependencyType.Default,
+  name: '@rx-angular/cdk',
+  version: '>=1.0.0-alpha.0',
+  overwrite: true,
+};
