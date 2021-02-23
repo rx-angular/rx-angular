b2719eeb2185c57a615d410403a601e4887d94dd
Vojtech Mašek <vojtech.masek@flowup.cz>
2021-02-23 15:35:25 +0100

    refactor(schematics): dist pre publish cleanup

--- libs/template/schematics/src/commands/ng-add/index.ts
+++ libs/template/schematics/src/commands/ng-add/index.ts
@@ -2,98 +2,90 @@ import {
   chain,
   Rule,
   SchematicContext,
-  SchematicsException,
   Tree,
 } from '@angular-devkit/schematics';
-import {
-  addImportToModule,
-  insertImport,
-} from '@schematics/angular/utility/ast-utils';
 import {
   addPackageJsonDependency,
   NodeDependency,
 } from '@schematics/angular/utility/dependencies';
-import * as ts from 'typescript';
 
 import {
   findRootModule,
   getLatestNodePackage,
   getProject,
-  InsertChange,
-  packageName,
   peerDependencies,
 } from '../../common';
 import { SchemaOptions } from './schema';
 import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-function getModuleFile(tree: Tree, options: SchemaOptions): ts.SourceFile {
-  const modulePath = options.module;
+// function getModuleFile(tree: Tree, options: SchemaOptions): ts.SourceFile {
+//   const modulePath = options.module;
+//
+//   if (!tree.exists(modulePath)) {
+//     throw new SchematicsException(`File ${modulePath} does not exist.`);
+//   }
+//
+//   const text = tree.read(modulePath);
+//   if (text === null) {
+//     throw new SchematicsException(`File ${modulePath} does not exist.`);
+//   }
+//   const sourceText = text.toString('utf-8');
+//
+//   return ts.createSourceFile(
+//     modulePath,
+//     sourceText,
+//     ts.ScriptTarget.Latest,
+//     true
+//   );
+// }
 
-  if (!tree.exists(modulePath)) {
-    throw new SchematicsException(`File ${modulePath} does not exist.`);
-  }
+// function applyChanges(tree: Tree, path: string, changes: InsertChange[]): Tree {
+//   const recorder = tree.beginUpdate(path);
+//
+//   for (const change of changes) {
+//     recorder.insertLeft(change.pos, change.toAdd);
+//   }
+//
+//   tree.commitUpdate(recorder);
+//
+//   return tree;
+// }
 
-  const text = tree.read(modulePath);
-  if (text === null) {
-    throw new SchematicsException(`File ${modulePath} does not exist.`);
-  }
-  const sourceText = text.toString('utf-8');
+// function addImportsToModuleFile(
+//   options: SchemaOptions,
+//   imports: string[],
+//   importName = packageName
+// ): Rule {
+//   return (tree) => {
+//     const module = getModuleFile(tree, options);
+//     const importChanges = insertImport(
+//       module,
+//       options.module,
+//       imports.join(', '),
+//       importName
+//     );
+//
+//     return applyChanges(tree, options.module, [
+//       importChanges,
+//     ] as InsertChange[]);
+//   };
+// }
 
-  return ts.createSourceFile(
-    modulePath,
-    sourceText,
-    ts.ScriptTarget.Latest,
-    true
-  );
-}
-
-function applyChanges(tree: Tree, path: string, changes: InsertChange[]): Tree {
-  const recorder = tree.beginUpdate(path);
-
-  for (const change of changes) {
-    recorder.insertLeft(change.pos, change.toAdd);
-  }
-
-  tree.commitUpdate(recorder);
-
-  return tree;
-}
-
-function addImportsToModuleFile(
-  options: SchemaOptions,
-  imports: string[],
-  importName = packageName
-): Rule {
-  return (tree) => {
-    const module = getModuleFile(tree, options);
-    const importChanges = insertImport(
-      module,
-      options.module,
-      imports.join(', '),
-      importName
-    );
-
-    return applyChanges(tree, options.module, [
-      importChanges,
-    ] as InsertChange[]);
-  };
-}
-
-function addImportsToModuleDeclaration(
-  options: SchemaOptions,
-  imports: string[]
-): Rule {
-  return (tree) => {
-    const module = getModuleFile(tree, options);
-
-    const importChanges = imports.map(
-      (imp) => addImportToModule(module, options.module, imp, packageName)[0]
-    );
-
-    return applyChanges(tree, options.module, importChanges as InsertChange[]);
-  };
-}
+// function addImportsToModuleDeclaration(
+//   options: SchemaOptions,
+//   imports: string[]
+// ): Rule {
+//   return (tree) => {
+//     const module = getModuleFile(tree, options);
+//
+//     const importChanges = imports.map(
+//       (imp) => addImportToModule(module, options.module, imp, packageName)[0]
+//     );
+//
+//     return applyChanges(tree, options.module, importChanges as InsertChange[]);
+//   };
+// }
 
 function addPackageJsonDependencies(
   packages: (string | NodeDependency)[]
@@ -122,14 +114,13 @@ export function ngAdd(options: SchemaOptions): Rule {
   return async (tree: Tree) => {
     const project = await getProject(tree, options.project);
     const sourceRoot = (project && project.sourceRoot) ?? 'src';
-    const modulesToAdd = ['LetModule', 'PushModule'];
 
     options.module = findRootModule(tree, options.module, sourceRoot) as string;
 
     return chain([
       addPackageJsonDependencies(peerDependencies),
-      addImportsToModuleFile(options, modulesToAdd),
-      addImportsToModuleDeclaration(options, modulesToAdd),
+      // addImportsToModuleFile(options, modulesToAdd),
+      // addImportsToModuleDeclaration(options, modulesToAdd),
     ]);
   };
 }
