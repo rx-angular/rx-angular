8541b09066678bf0921e8637317c206af89670ed
Michael Hladky <michael@hladky.at>
2021-02-23 17:02:04 +0100

    release(template): 1.0.0-beta-19

--- libs/template/schematics/src/common/utils/npmjs.ts
+++ libs/template/schematics/src/common/utils/npmjs.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { fromPromise } from 'rxjs/internal-compatibility';
 import { map, take } from 'rxjs/operators';
 import fetch from 'node-fetch';
+
 export interface NpmPackage {
   name: string;
   version: string;
@@ -13,7 +14,9 @@ export interface NpmPackage {
 
 export function getLatestNodePackage(name: string): Observable<NodeDependency> {
   return fromPromise(
-    fetch(`https://registry.npmjs.org/${name}/latest`).then((res) => res.json())
+    fetch(`https://registry.npmjs.org/${name}/latest`).then(
+      (res: any): NpmPackage => res.json()
+    )
   ).pipe(
     map((packageData: NpmPackage) => ({
       type: NodeDependencyType.Default,
