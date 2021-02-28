c27e972caf709c613223d9fb83e256a9e20e3264
Michael Hladky <michael@hladky.at>
2021-02-28 03:30:41 +0100

    fix(demos): reduce examples

--- apps/demos/src/app/features/concepts/fundamentals.menu.ts
+++ apps/demos/src/app/features/concepts/fundamentals.menu.ts
@@ -11,22 +11,22 @@ export const FUNDAMENTALS_MENU = [
     link: 'passing-values',
     label: 'Passing Values'
   },
-  {
+  /*{
     link: 'zone-patched-apis',
     label: 'Zone Patched APIs'
   },
   {
     label: 'Scheduling',
     link: 'scheduling'
-  },
+  },*/
   {
     label: 'Coalescing',
     link: 'coalescing'
   },
-  {
+  /*{
     label: 'Global Order',
     link: 'global-order'
-  },
+  },*/
   {
     link: 'view-vs-embedded-view',
     label: 'Component vs EmbeddedView',
--- apps/demos/src/app/features/performance/performance-shell.menu.ts
+++ apps/demos/src/app/features/performance/performance-shell.menu.ts
@@ -1,9 +1,7 @@
 import { RXLET_VS_PUSH_MENU_ITEMS } from './rx-let-vs-push/rx-let-vs-push.menu';
-import { ALPHAS_COMPARE_MENU_ITEMS } from './alphas-compare/alphas-compare.menu';
 
 export const MENU_ITEMS = [
   ...RXLET_VS_PUSH_MENU_ITEMS,
-  ...ALPHAS_COMPARE_MENU_ITEMS,
   {
     label: 'Nested Component Structure',
     link: 'nested-component-structure'
