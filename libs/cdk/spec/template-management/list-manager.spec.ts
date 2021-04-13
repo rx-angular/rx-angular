64314b01e68191f9a58677acb44aefba2fc40fd1
Julian <julian@jandl.wien>
2021-04-13 18:52:41 +0200

    test(cdk): implement first tests for list-manager covering basics and exception handling

---
+++ libs/cdk/spec/template-management/list-manager.spec.ts
@@ -0,0 +1,221 @@
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EmbeddedViewRef,
+  ErrorHandler, Input,
+  IterableDiffers,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+// tslint:disable-next-line:nx-enforce-module-boundaries
+import {
+  createListTemplateManager,
+  RX_ANGULAR_CONFIG,
+  RxDefaultListViewContext,
+  RxListManager,
+  RxListViewComputedContext,
+  RxListViewContext,
+  RxStrategyProvider,
+} from '@rx-angular/cdk';
+// tslint:disable-next-line:nx-enforce-module-boundaries
+import { mockConsole } from '@test-helpers';
+import { ReplaySubject } from 'rxjs';
+import createSpy = jasmine.createSpy;
+
+@Component({
+  selector: 'rx-angular-error-test',
+  template: `{{value}}`
+})
+class ErrorTestComponent {
+  private _value: number;
+  @Input()
+  set value(value: number) {
+    this._value = value;
+    if (value %2 === 0) {
+      throw new Error('isEven');
+    }
+  }
+  get value(): number {
+    return this._value;
+  }
+}
+
+@Component({
+  template: `
+    <ng-template #tmpl let-v><rx-angular-error-test [value]="v"></rx-angular-error-test></ng-template>
+    <span #host></span>
+  `,
+})
+class ListTemplateManagerSpecComponent implements AfterViewInit {
+  @ViewChild('tmpl', { read: TemplateRef })
+  templateRef: TemplateRef<RxDefaultListViewContext<number>>;
+
+  @ViewChild('host', { read: ViewContainerRef })
+  host: ViewContainerRef;
+
+  listManager: RxListManager<number>;
+
+  values$ = new ReplaySubject<number[]>(1);
+
+  latestRenderedValue: any;
+
+  constructor(
+    private iterableDiffers: IterableDiffers,
+    private cdRef: ChangeDetectorRef,
+    private eRef: ElementRef,
+    private vcRef: ViewContainerRef,
+    private strategyProvider: RxStrategyProvider,
+    public errorHandler: ErrorHandler
+  ) {}
+
+  ngAfterViewInit() {
+    this.listManager = createListTemplateManager<
+      any,
+      RxDefaultListViewContext<number>
+    >({
+      iterableDiffers: this.iterableDiffers,
+      renderSettings: {
+        cdRef: this.cdRef,
+        eRef: this.eRef,
+        strategies: this.strategyProvider.strategies as any, // TODO: move strategyProvider
+        defaultStrategyName: this.strategyProvider.primaryStrategy,
+        parent: false,
+        patchZone: false,
+        errorHandler: this.errorHandler,
+      },
+      templateSettings: {
+        viewContainerRef: this.host,
+        templateRef: this.templateRef,
+        createViewContext,
+        updateViewContext,
+      },
+      trackBy: (idx, item) => item,
+    });
+    this.listManager.render(this.values$).subscribe((v: any) => {
+      this.latestRenderedValue = v;
+    });
+  }
+}
+
+/** @internal */
+const createViewContext = (
+  item: any,
+  computedContext: RxListViewComputedContext
+): RxDefaultListViewContext<number> => {
+  return new RxDefaultListViewContext<number>(item, computedContext);
+};
+
+/** @internal */
+const updateViewContext = (
+  item: number,
+  view: EmbeddedViewRef<RxListViewContext<number>>,
+  computedContext: RxListViewComputedContext
+): void => {
+  view.context.updateContext(computedContext);
+  view.context.$implicit = item;
+};
+
+const customErrorHandler: ErrorHandler = {
+  handleError: createSpy('handleError')
+}
+
+let fixtureComponent: any;
+let componentInstance: {
+  listManager: RxListManager<any>;
+  errorHandler: ErrorHandler;
+  templateRef: TemplateRef<any>;
+  values$: ReplaySubject<any[]>;
+  latestRenderedValue: any;
+};
+let componentNativeElement: any;
+const setupListManagerComponent = (): void => {
+  TestBed.configureTestingModule({
+    declarations: [ListTemplateManagerSpecComponent, ErrorTestComponent],
+    providers: [
+      { provide: ErrorHandler, useValue: customErrorHandler},
+      ViewContainerRef,
+      {
+        provide: RX_ANGULAR_CONFIG,
+        useValue: {
+          primaryStrategy: 'native',
+        },
+      },
+    ],
+  });
+
+  fixtureComponent = TestBed.createComponent(ListTemplateManagerSpecComponent);
+  componentInstance = fixtureComponent.componentInstance;
+  componentNativeElement = fixtureComponent.nativeElement;
+};
+
+describe('list-manager', () => {
+  beforeAll(() => mockConsole());
+
+  beforeEach(waitForAsync(setupListManagerComponent));
+
+  it('should be created', () => {
+    expect(fixtureComponent).toBeTruthy();
+  });
+
+  it('should have customErrorHandler', () => {
+    expect(componentInstance.errorHandler).toEqual(customErrorHandler);
+  });
+
+  it('should have listManager', () => {
+    fixtureComponent.detectChanges();
+    expect(componentInstance.listManager).toBeTruthy();
+  });
+
+  it('should have templateRef', () => {
+    fixtureComponent.detectChanges();
+    expect(componentInstance.templateRef).toBeTruthy();
+  });
+
+  it('should render items', () => {
+    fixtureComponent.detectChanges();
+    componentInstance.values$.next([1]);
+    fixtureComponent.detectChanges();
+    const componentContent = componentNativeElement.textContent;
+    expect(componentContent).toEqual('1')
+  });
+
+  describe('exception handling', () => {
+
+    it('should capture errors with errorHandler', () => {
+      fixtureComponent.detectChanges();
+      componentInstance.values$.next([2]);
+      try {
+        fixtureComponent.detectChanges();
+      } catch (e) {}
+      expect(customErrorHandler.handleError).toHaveBeenCalled();
+    });
+
+    it('should emit error and payload via renderCallback', () => {
+      fixtureComponent.detectChanges();
+      const items = [2];
+      componentInstance.values$.next(items);
+      try {
+        fixtureComponent.detectChanges();
+      } catch (e) {
+        expect(componentInstance.latestRenderedValue[0]).toEqual(e);
+      }
+    });
+
+    it('should work after an error has been thrown', () => {
+      fixtureComponent.detectChanges();
+      componentInstance.values$.next([2]);
+      try {
+        fixtureComponent.detectChanges();
+      } catch (e) {}
+      componentInstance.values$.next([1,3]);
+      fixtureComponent.detectChanges();
+      const componentContent = componentNativeElement.textContent;
+      expect(componentContent).toEqual('13');
+    });
+  })
+
+});
