375096a5ecc8937f345f737030495b9fcd324083
Julian <julian@jandl.wien>
2021-04-13 13:21:15 +0200

    feat(cdk): make sure isRxRenderError works properly. mark RenderError as internal

--- libs/cdk/src/lib/template-management/render-error.ts
+++ libs/cdk/src/lib/template-management/render-error.ts
@@ -1,29 +1,41 @@
 import { ErrorHandler } from '@angular/core';
 
+/** @internal **/
 export type RxRenderError<T> = [Error, T];
 
-export type RxRenderErrorFactory<T, E> = (error: Error, value: T) => RxRenderError<E>;
+/** @internal **/
+export type RxRenderErrorFactory<T, E> = (
+  error: Error,
+  value: T
+) => RxRenderError<E>;
 
+/** @internal **/
 export function isRxRenderError<T>(e: any): e is RxRenderError<T> {
-  return e != null && Array.isArray(e);
+  return (
+    e != null && Array.isArray(e) && e.length === 2 && e[0] instanceof Error
+  );
 }
 
+/** @internal **/
 export function createDefaultErrorHandler(
   _handler?: ErrorHandler
 ): ErrorHandler {
-  const _handleError = _handler ? e => _handler.handleError(e) : console.error;
+  const _handleError = _handler
+    ? (e) => _handler.handleError(e)
+    : console.error;
   return {
-    handleError: error => {
+    handleError: (error) => {
       if (isRxRenderError(error)) {
         _handleError(error[0]);
         console.error('additionalErrorContext', error[1]);
       } else {
         _handleError(error);
       }
-    }
-  }
+    },
+  };
 }
 
+/** @internal **/
 export function toRenderError<T>(e: Error, context: T): RxRenderError<T> {
   return [e, context];
 }
