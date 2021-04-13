07791b4d7d075493d7253c9dc5fa1f5ac7aeefd4
Julian <julian@jandl.wien>
2021-04-13 14:02:16 +0200

    feat(demos): implement rx-let exception handling demo compared to native async pipe

---
+++ apps/demos/src/app/features/template/rx-let/exception-handling/error-handling-child.component.ts
@@ -0,0 +1,38 @@
+import {
+  ChangeDetectionStrategy,
+  Component, DoCheck, ElementRef,
+  Input,
+  OnInit,
+} from '@angular/core';
+
+@Component({
+  selector: 'rxa-error-handling-child',
+  template: `{{ _index }}<ng-content></ng-content>`,
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ErrorHandlingChildComponent implements OnInit, DoCheck {
+  _index: number;
+  @Input() set index(index: number) {
+    console.log(index);
+    this._index = index;
+    if (index > 5) {
+      throw new Error('erororororo');
+    }
+  }
+
+  private removed = false;
+
+  constructor(
+    private el: ElementRef
+  ) {}
+
+  ngOnInit(): void {}
+
+  ngDoCheck() {
+    /*if (this._index %2 !== 0 && !this.removed && this.el.nativeElement.parentElement) {
+      this.el.nativeElement.parentElement.removeChild(this.el.nativeElement);
+      this.removed = true;
+    }*/
+  }
+}
