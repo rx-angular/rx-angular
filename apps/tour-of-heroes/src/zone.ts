a2e97301c82a46a1330034900d8a9e205efd873d
Michael Hladky <michael@hladky.at>
2021-05-25 22:46:46 +0200

    docs(tour-of-heroes):  move into separate repository (#788)

--- apps/tour-of-heroes/src/zone.ts
+++
@@ -1,4 +0,0 @@
-/***************************************************************************************************
- * Zone JS is required by default for Angular itself.
- */
-import 'zone.js'; // Included with Angular CLI.
