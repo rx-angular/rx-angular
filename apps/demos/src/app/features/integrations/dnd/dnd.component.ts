9e70119ddbebb8cfcee37c41925466f1790d1f66
Julian Jandl <julian@jandl.wien>
2021-07-06 13:11:35 +0200

    feat(demos): prove that ngFor has the same issue, but rxFor can handle it with noop-strategy

--- apps/demos/src/app/features/integrations/dnd/dnd.component.ts
+++ apps/demos/src/app/features/integrations/dnd/dnd.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { delay, map, shareReplay, tap } from 'rxjs/operators';
 import { RxForModule } from '../../../rx-angular-pocs/template/directives/for/rx-for.module';
 
 @Component({
@@ -18,6 +18,51 @@ import { RxForModule } from '../../../rx-angular-pocs/template/directives/for/rx
         <li
           cdkDrag
           *ngFor="let item of items$ | async"
+        >{{item.value}}</li>
+      </ul>
+    </div>
+
+    <div
+      cdkDropList
+      (cdkDropListDropped)="drop($event)"
+    >
+      <h3>ngFor with error</h3>
+      <ul>
+        <li
+          cdkDrag
+          (cdkDragStarted)="dragging$.next(true)"
+          (cdkDragEnded)="dragging$.next(false)"
+          *ngFor="let item of errorItems$ | async; trackBy: trackItem"
+        >{{item.value}}</li>
+      </ul>
+    </div>
+
+    <div
+      cdkDropList
+      (cdkDropListDropped)="drop($event)"
+    >
+      <h3>rxFor errorItems with</h3>
+      <ul>
+        <li
+          cdkDrag
+          (cdkDragStarted)="dragging$.next(true)"
+          (cdkDragEnded)="dragging$.next(false)"
+          *rxFor="let item of errorItems$; trackBy: trackItem; strategy: 'normal'"
+        >{{item.value}}</li>
+      </ul>
+    </div>
+
+    <div
+      cdkDropList
+      (cdkDropListDropped)="drop($event)"
+    >
+      <h3>rxFor errorItems with noop-switch</h3>
+      <ul>
+        <li
+          cdkDrag
+          (cdkDragStarted)="dragging$.next(true)"
+          (cdkDragEnded)="dragging$.next(false)"
+          *rxFor="let item of errorItems$; trackBy: trackItem; strategy: noopWhileDrag$"
         >{{item.value}}</li>
       </ul>
     </div>
@@ -67,7 +112,34 @@ import { RxForModule } from '../../../rx-angular-pocs/template/directives/for/rx
 })
 export class DndComponent {
   items$ = new BehaviorSubject<any>(createItems(50));
+  private errorItems = [
+    {
+      id: 1,
+      value: 'item - 1'
+    },
+    {
+      id: 2,
+      value: 'item - 2'
+    }
+  ];
+  private errorItemsSwapped = [
+    {
+      id: 2,
+      value: 'item - 2'
+    },
+    {
+      id: 1,
+      value: 'item - 1'
+    }
+  ];
+  _errorItems$ = new BehaviorSubject<any>(this.errorItems);
   dragging$ = new BehaviorSubject<boolean>(false);
+  errorItems$ = this.dragging$.pipe(
+    delay(1000),
+    map(d => !d ? [...this.errorItems] : [...this.errorItemsSwapped]),
+    tap(data => console.log('items', data)),
+    shareReplay({ bufferSize: 1, refCount: true })
+  )
 
   noopWhileDrag$ = this.dragging$.pipe(
     map(d => d ? 'noop' : 'normal')
