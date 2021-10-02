5629544be2c71eb25754b92223bff6b05f6c4198
Julian Jandl <julian@jandl.wien>
2021-10-02 11:55:11 +0200

    chore: wip deprecated cdk exports

--- libs/cdk/src/index.ts
+++ libs/cdk/src/index.ts
@@ -1,5 +1,55 @@
-export * from '@rx-angular/cdk/coalescing';
-export * from '@rx-angular/cdk/coercing';
+import {RxCoalescingOptions as ɵRxCoalescingOptions,
+  coalescingObj as ɵcoalescingObj, coalesceWith as ɵcoalesceWith, coalescingManager as ɵcoalescingManager} from '@rx-angular/cdk/coalescing';
+/**
+ * @deprecated import `RxCoalescingOptions` from `@rx-angular/cdk/coalescing`
+ */
+type RxCoalescingOptions = ɵRxCoalescingOptions;
+/**
+ * @deprecated import `coalescingObj` from `@rx-angular/cdk/coalescing`
+ */
+type coalescingObj = ɵcoalescingObj;
+/**
+ * @deprecated import `coalesceWith` from `@rx-angular/cdk/coalescing`
+ */
+const coalesceWith = ɵcoalesceWith;
+/**
+ * @deprecated import `coalescingManager` from `@rx-angular/cdk/coalescing`
+ */
+const coalescingManager = ɵcoalescingManager;
+
+export { RxCoalescingOptions, coalescingObj, coalesceWith, coalescingManager };
+
+import {
+  coerceObservable as ɵcoerceObservable,
+  coerceObservableWith as ɵcoerceObservableWith,
+  coerceDistinctObservable as ɵcoerceDistinctObservable,
+  coerceDistinctWith as ɵcoerceDistinctWith,
+  coerceAllFactory as ɵcoerceAllFactory,
+} from '@rx-angular/cdk/coercing';
+
+/**
+ * @deprecated import `coerceObservable` from `@rx-angular/cdk/coercing`
+ */
+const coerceObservable = ɵcoerceObservable;
+/**
+ * @deprecated import `coerceObservableWith` from `@rx-angular/cdk/coercing`
+ */
+const coerceObservableWith = ɵcoerceObservableWith;
+/**
+ * @deprecated import `coerceDistinctObservable` from `@rx-angular/cdk/coercing`
+ */
+const coerceDistinctObservable = ɵcoerceDistinctObservable;
+/**
+ * @deprecated import `coerceDistinctWith` from `@rx-angular/cdk/coercing`
+ */
+const coerceDistinctWith = ɵcoerceDistinctWith;
+/**
+ * @deprecated import `coerceAllFactory` from `@rx-angular/cdk/coercing`
+ */
+const coerceAllFactory = ɵcoerceAllFactory;
+
+export { coerceObservable, coerceObservableWith, coerceDistinctObservable, coerceDistinctWith, coerceAllFactory };
+
 export * from '@rx-angular/cdk/internals/scheduler';
 export * from '@rx-angular/cdk/render-strategies';
 export * from '@rx-angular/cdk/state';
