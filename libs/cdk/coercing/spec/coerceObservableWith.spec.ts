7e68406d0e6c75d1937aebb0f799c91f0c1ddb4b
Michael Huynh <miqid@outlook.com>
2021-10-11 21:29:32 +0800

    test(cdk): add basic tests for coerceObservableWith (#969)
    Adds some basic marble tests to cover the `coerceObservableWith`
    operator.

---
+++ libs/cdk/coercing/spec/coerceObservableWith.spec.ts
@@ -0,0 +1,66 @@
+import { coerceObservableWith } from '@rx-angular/cdk';
+import { jestMatcher, mockConsole } from '@test-helpers';
+import { of } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
+
+describe('coerceObservableWith', () => {
+  beforeAll(() => mockConsole());
+
+  let testScheduler: TestScheduler;
+
+  beforeEach(() => {
+    testScheduler = new TestScheduler(jestMatcher);
+  });
+
+  it('should coerce non-observable values', () => {
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const src = cold('a---b---c|  ');
+      const subs = '    ^--------!  ';
+      const expected = 'a---b---c|  ';
+      const a = cold('  (a|)        ');
+      const b = cold('      (b|)    ');
+      const c = cold('          (c|)');
+      const expectedValues = { a, b, c };
+
+      const result = src.pipe(coerceObservableWith());
+      
+      expectObservable(result).toBe(expected, expectedValues);
+      expectSubscriptions(src.subscriptions).toBe(subs);
+    });
+  });
+
+  it('should preserve observable values', () => {
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const values = { a: of('a'), b: of('b'), c: of('c') };
+      const src = cold('a---b---c|  ', values);
+      const subs = '    ^--------!  ';
+      const expected = 'a---b---c|  ';
+      const a = cold('  (a|)        ');
+      const b = cold('      (b|)    ');
+      const c = cold('          (c|)');
+      const expectedValues = { a, b, c };
+
+      const result = src.pipe(coerceObservableWith());
+      
+      expectObservable(result).toBe(expected, expectedValues);
+      expectSubscriptions(src.subscriptions).toBe(subs);
+    });
+  });
+
+  it('should propagate error', () => {
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const values = { a: 'a' };
+      const errorValue = 'error';
+      const src = cold('a---#', values, errorValue);
+      const subs = '    ^---!';
+      const expected = 'a---#';
+      const a = cold('  (a|)');
+      const expectedValues = { a };
+
+      const result = src.pipe(coerceObservableWith());
+
+      expectObservable(result).toBe(expected, expectedValues, errorValue);
+      expectSubscriptions(src.subscriptions).toBe(subs);
+    });
+  });
+});
