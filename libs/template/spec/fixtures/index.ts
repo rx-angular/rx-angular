9c3ab306f868ac4f152f2fad871bebaba708185d
Michael Hladky <michael@hladky.at>
2021-10-27 15:51:18 +0200

    perf: reduce bundle size by removing unused code

--- libs/template/spec/fixtures/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './fixtures';
-export * from './emissions';
-export * from './mock-strategies';
-export * from './mock-event-emitter';
-export * from './mock-ng-zone';
-export * from './mock-noop-ng-zone';
--- libs/template/src/test-setup.ts
+++
@@ -1,2 +0,0 @@
-import 'jest-preset-angular/setup-jest';
-window.MessageChannel = require('worker_threads').MessageChannel;
