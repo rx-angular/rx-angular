166b365fb5c3d30c92667988a69e82660fa19ec6
Michael Hladky <michael@hladky.at>
2021-11-17 04:21:09 +0100

    chore: setup bundle-budgets apps

---
+++ apps/bundle-budget--template-unpatch/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      stringifyContentPathRegex: '\\.(html|svg)$',
+
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/vanilla-case-studies',
+
+  displayName: 'vanilla-case-studies',
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+  transform: { '^.+\\.(ts|js|html)$': 'jest-preset-angular' },
+};
---
+++ apps/bundle-budget--template-unpatch/src/main.ts
@@ -0,0 +1,14 @@
+import('./empty-bundle').then(m => m);
+import('@rx-angular/template/unpatch').then(m => m.UnpatchModule);
+import('@rx-angular/template/let').then(m => m.LetModule);
+import('@rx-angular/template/push').then(m => m.PushModule);
+
+import('@rx-angular/cdk/coalescing').then(m => m);
+import('@rx-angular/cdk/coercing').then(m => m);
+import('@rx-angular/cdk/notifications').then(m => m);
+import('@rx-angular/cdk/render-strategies').then(m => m);
+import('@rx-angular/cdk/state').then(m => m);
+import('@rx-angular/cdk/template').then(m => m);
+import('@rx-angular/cdk/template').then(m => m);
+import('@rx-angular/cdk/zone-less').then(m => m);
+import('@rx-angular/cdk/zone-configurations').then(m => m.zoneConfig);
