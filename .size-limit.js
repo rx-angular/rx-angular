aaa8308fa2cd7138523cf6c740e5fb72cfae7b3a
Michael Hladky <michael@hladky.at>
2021-11-17 04:21:29 +0100

    chore: ass size-limit

---
+++ .size-limit.js
@@ -0,0 +1,47 @@
+module.exports = [
+  /*
+  {
+    path: 'dist/apps/bundle-budget--template-unpatch/apps_bundle-budget--template-unpatch_src_empty-bundle_ts.js',
+    limit: '10ms',
+  },
+  {
+    path: 'dist/apps/bundle-budget--template-unpatch/libs_template_unpatch_src_index_ts.js',
+    limit: '100ms',
+  },
+  {
+    path: 'dist/apps/bundle-budget--template-unpatch/libs_template_let_src_index_ts.js',
+    limit: '100ms',
+  },
+  {
+    path: 'dist/apps/bundle-budget--template-unpatch/libs_template_push_src_index_ts.js',
+    limit: '100ms',
+  },
+  {
+    path: 'dist/apps/bundle-budget--template-unpatch/libs_cdk_coalescing_src_index_ts.js',
+    limit: '100ms',
+  },
+  {
+    path: 'dist/apps/bundle-budget--template-unpatch/libs_cdk_coercing_src_index_ts.js',
+    limit: '100ms',
+  },
+  {
+    path: 'dist/apps/bundle-budget--template-unpatch/default-libs_cdk_render-strategies_src_index_ts.js',
+    limit: '100ms',
+  },
+  {
+    path: 'dist/apps/bundle-budget--template-unpatch/libs_cdk_state_src_index_ts.js',
+    limit: '100ms',
+  },
+  {
+    path: 'dist/apps/bundle-budget--template-unpatch/default-libs_cdk_template_src_index_ts.js',
+    limit: '100ms',
+  },*/
+  {
+    path: 'dist/apps/bundle-budget--template-unpatch/default-libs_cdk_zone-less_src_index_ts.js',
+    limit: '307ms',
+  },
+  {
+    path: 'dist/apps/bundle-budget--template-unpatch/main.js',
+    limit: '270ms',
+  },
+];
