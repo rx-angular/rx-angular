d3e97214119318ab6ba8b5358f7bd9a7b361a4d4
Julian Jandl <julian@jandl.wien>
2021-11-19 21:54:43 +0100

    chore: setup missing migration tests, remove compat layer

--- libs/cdk/schematics/src/migrations/update-1-0-0/index.spec.ts
+++ libs/cdk/schematics/src/migrations/update-1-0-0/index.spec.ts
@@ -46,6 +46,318 @@ describe('cdk migration 1.0.0', () => {
     expect(file).toMatchSnapshot();
   });
 
+  it('should replace coercing', async () => {
+    appTree = await setupTestFile(`
+      import { NgModule } from '@angular/core';
+      import { BrowserModule } from '@angular/platform-browser';
+      import {
+        coerceObservable,
+        coerceObservableWith,
+        coerceDistinctObservable,
+        coerceDistinctWith,
+        coerceAllFactory
+      } from '@rx-angular/cdk';
+
+      import { AppComponent } from './app.component';
+
+      @NgModule({
+        declarations: [
+          AppComponent,
+        ],
+        imports: [
+          BrowserModule
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
+      })
+      export class AppModule { }
+  `);
+
+    const file = appTree.readContent('app.module.ts');
+
+    expect(file).toMatchSnapshot();
+  });
+
+  it('should replace internals-scheduler', async () => {
+    appTree = await setupTestFile(`
+      import { NgModule } from '@angular/core';
+      import { BrowserModule } from '@angular/platform-browser';
+      import {
+        cancelCallback,
+        scheduleCallback,
+        forceFrameRate,
+        PriorityLevel
+      } from '@rx-angular/cdk';
+
+      import { AppComponent } from './app.component';
+
+      @NgModule({
+        declarations: [
+          AppComponent,
+        ],
+        imports: [
+          BrowserModule
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
+      })
+      export class AppModule { }
+  `);
+
+    const file = appTree.readContent('app.module.ts');
+
+    expect(file).toMatchSnapshot();
+  });
+
+  it('should replace render-strategies', async () => {
+    appTree = await setupTestFile(`
+      import { NgModule } from '@angular/core';
+      import { BrowserModule } from '@angular/platform-browser';
+      import {
+        ScheduleOnStrategyOptions,
+        RX_CONCURRENT_STRATEGIES,
+        RxConcurrentStrategies,
+        RX_NATIVE_STRATEGIES,
+        RxNativeStrategies,
+        onStrategy,
+        strategyHandling,
+        RxStrategies,
+        RxStrategyNames,
+        RxDefaultStrategyNames,
+        RxConcurrentStrategyNames,
+        RxNativeStrategyNames,
+        RxCustomStrategyCredentials,
+        RxStrategyCredentials,
+        RxRenderBehavior,
+        RxRenderWork,
+        RX_ANGULAR_CONFIG,
+        RxAngularConfig
+      } from '@rx-angular/cdk';
+
+      import { AppComponent } from './app.component';
+
+      @NgModule({
+        declarations: [
+          AppComponent,
+        ],
+        imports: [
+          BrowserModule
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
+      })
+      export class AppModule { }
+  `);
+
+    const file = appTree.readContent('app.module.ts');
+
+    expect(file).toMatchSnapshot();
+  });
+
+  it('should replace state', async () => {
+    appTree = await setupTestFile(`
+      import { NgModule } from '@angular/core';
+      import { BrowserModule } from '@angular/platform-browser';
+      import {
+        ObservableAccumulation,
+        ObservableMap,
+        accumulateObservables
+      } from '@rx-angular/cdk';
+
+      import { AppComponent } from './app.component';
+
+      @NgModule({
+        declarations: [
+          AppComponent,
+        ],
+        imports: [
+          BrowserModule
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
+      })
+      export class AppModule { }
+  `);
+
+    const file = appTree.readContent('app.module.ts');
+
+    expect(file).toMatchSnapshot();
+  });
+
+  it('should replace template', async () => {
+    appTree = await setupTestFile(`
+      import { NgModule } from '@angular/core';
+      import { BrowserModule } from '@angular/platform-browser';
+      import {
+        templateHandling,
+        RxBaseTemplateNames,
+        RxRenderAware,
+        RxViewContext,
+        rxBaseTemplateNames,
+        RxTemplateManager,
+        createTemplateManager,
+        RxNotificationTemplateNameMap,
+        RxListManager,
+        createListTemplateManager,
+        RxListViewComputedContext,
+        RxDefaultListViewContext,
+        RxListViewContext
+      } from '@rx-angular/cdk';
+
+      import { AppComponent } from './app.component';
+
+      @NgModule({
+        declarations: [
+          AppComponent,
+        ],
+        imports: [
+          BrowserModule
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
+      })
+      export class AppModule { }
+  `);
+
+    const file = appTree.readContent('app.module.ts');
+
+    expect(file).toMatchSnapshot();
+  });
+
+  it('should replace notifications', async () => {
+    appTree = await setupTestFile(`
+      import { NgModule } from '@angular/core';
+      import { BrowserModule } from '@angular/platform-browser';
+      import {
+        RxNotificationKind,
+        RxNotification,
+        RxCompleteNotification,
+        RxErrorNotification,
+        RxNextNotification,
+        RxNotificationValue,
+        RxSuspenseNotification,
+        toRxErrorNotification,
+        toRxSuspenseNotification,
+        toRxCompleteNotification,
+        templateTriggerHandling,
+        rxMaterialize,
+        createTemplateNotifier
+      } from '@rx-angular/cdk';
+
+      import { AppComponent } from './app.component';
+
+      @NgModule({
+        declarations: [
+          AppComponent,
+        ],
+        imports: [
+          BrowserModule
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
+      })
+      export class AppModule { }
+  `);
+
+    const file = appTree.readContent('app.module.ts');
+
+    expect(file).toMatchSnapshot();
+  });
+
+  it('should replace zone-less', async () => {
+    appTree = await setupTestFile(`
+      import { NgModule } from '@angular/core';
+      import { BrowserModule } from '@angular/platform-browser';
+      import {
+        getZoneUnPatchedApi,
+        Promise,
+        requestAnimationFrame,
+        cancelAnimationFrame,
+        setInterval,
+        clearInterval,
+        setTimeout,
+        clearTimeout,
+        unpatchAddEventListener,
+        interval,
+        timer,
+        fromEvent,
+        asyncScheduler,
+        asapScheduler,
+        queueScheduler,
+        animationFrameScheduler
+      } from '@rx-angular/cdk';
+
+      import { AppComponent } from './app.component';
+
+      @NgModule({
+        declarations: [
+          AppComponent,
+        ],
+        imports: [
+          BrowserModule
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
+      })
+      export class AppModule { }
+  `);
+
+    const file = appTree.readContent('app.module.ts');
+
+    expect(file).toMatchSnapshot();
+  });
+
+  it('should replace zone-configurations', async () => {
+    appTree = await setupTestFile(`
+      import { NgModule } from '@angular/core';
+      import { BrowserModule } from '@angular/platform-browser';
+      import {
+        focusEvents,
+        mouseEvents,
+        wheelEvents,
+        inputEvents,
+        formControlsEvents,
+        keyboardEvents,
+        vrEvents,
+        mSGestureEvents,
+        printEvents,
+        networkEvents,
+        audioEvents,
+        compositionEvents,
+        touchEvents,
+        globalEvents,
+        websocketEvents,
+        xhrEvents,
+        windowEvents,
+        allEvents,
+        EventTarget,
+        RxZoneFlagsHelperFunctions,
+        RxZoneGlobalConfigurations,
+        RxZoneTestConfigurations,
+        RxZoneRuntimeConfigurations,
+        zoneConfig
+      } from '@rx-angular/cdk';
+
+      import { AppComponent } from './app.component';
+
+      @NgModule({
+        declarations: [
+          AppComponent,
+        ],
+        imports: [
+          BrowserModule
+        ],
+        providers: [],
+        bootstrap: [AppComponent]
+      })
+      export class AppModule { }
+  `);
+
+    const file = appTree.readContent('app.module.ts');
+
+    expect(file).toMatchSnapshot();
+  });
+
   function setupTestFile(fileInput: string, filePath = './app.module.ts') {
     const runner = new SchematicTestRunner(
       '@rx-angular/cdk',
--- libs/cdk/src/test-setup.ts
+++
@@ -1,2 +0,0 @@
-// This should be commented until zone configuration is moved to a separate library.
-// import 'jest-preset-angular/setup-jest';
