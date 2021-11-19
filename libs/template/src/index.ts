2c008815ffb294e900f52052e15e87f4bf642ef2
Michael Hladky <michael@hladky.at>
2021-11-19 18:29:17 +0100

    fix: exports

--- libs/template/src/index.ts
+++ libs/template/src/index.ts
@@ -5,12 +5,7 @@ export {
   UnpatchDirective,
 } from '@rx-angular/template/unpatch';
 
-export { ViewportPrioModule } from './lib/experimental/viewport-prio/viewport-prio.module';
-export { ViewportPrioDirective } from './lib/experimental/viewport-prio/viewport-prio.experimental.directive';
-
-
 // STABLE
-// @deprecated import from template/unpatch
 // TODO(artur): we're keeping it here until we get a migration script.
 export { LetModule, LetDirective } from '@rx-angular/template/let';
 
