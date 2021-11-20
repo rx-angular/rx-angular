bb58da9dfab0a0e4d320d5b425a3e08a4aabad4f
Edouard Bozon <bozonedouard@gmail.com>
2021-11-20 12:30:16 +0100

    chore: rename migration

--- libs/template/schematics/src/migrations/update-1.0.0-beta.29/index.spec.ts
+++ libs/template/schematics/src/migrations/update-1.0.0-beta.29/index.spec.ts
@@ -5,7 +5,7 @@ import {
 } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
-describe('Template Migration 1.0.0', () => {
+describe('Template Migration 1.0.0-beta.29', () => {
   let appTree: UnitTestTree;
 
   /* Increase Jest timeout because of long living tests. */
@@ -161,6 +161,6 @@ describe('Template Migration 1.0.0', () => {
 
     tree.create(filePath, fileInput);
 
-    return runner.runSchematicAsync(`update-1-0-0`, {}, tree).toPromise();
+    return runner.runSchematicAsync(`update-1.0.0-beta.29`, {}, tree).toPromise();
   }
 });
