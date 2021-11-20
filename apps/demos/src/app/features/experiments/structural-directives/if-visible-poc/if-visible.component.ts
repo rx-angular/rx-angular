5e94c07809a001a70559f8826db542a07d84033c
Edouard Bozon <bozonedouard@gmail.com>
2021-11-20 15:45:27 +0100

    chore: format

--- apps/demos/src/app/features/experiments/structural-directives/if-visible-poc/if-visible.component.ts
+++ apps/demos/src/app/features/experiments/structural-directives/if-visible-poc/if-visible.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';;
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'rxa-rx-let-poc',
