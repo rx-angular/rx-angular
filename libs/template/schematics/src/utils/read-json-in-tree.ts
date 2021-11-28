2248930a4c61fe686ef3d3a25abd6161ade10e5b
Julian Jandl <julian@jandl.wien>
2021-11-28 23:23:51 +0100

    chore: fix build

--- libs/template/schematics/src/utils/read-json-in-tree.ts
+++ libs/template/schematics/src/utils/read-json-in-tree.ts
@@ -4,10 +4,14 @@ export function readJsonInTree<T = any>(host: Tree, path: string): T {
   if (!host.exists(path)) {
     throw new Error(`Cannot find ${path}`);
   }
-  const contents = host.read(path).toString('utf-8');
-  try {
-    return JSON.parse(contents);
-  } catch (e) {
-    throw new Error(`Cannot parse ${path}: ${e.message}`);
+  const buffer = host.read(path);
+  if (buffer != null) {
+    const contents = buffer.toString('utf-8');
+    try {
+      return JSON.parse(contents);
+    } catch (e) {
+      throw new Error(`Cannot parse ${path}: ${e.message}`);
+    }
   }
+  throw new Error(`cannot read ${path}`);
 }
