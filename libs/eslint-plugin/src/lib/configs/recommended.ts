f4661bd813897f0cad3eed501a5bd1ee4063982b
Matěj Chalk <matejchalk@gmail.com>
2021-12-08 15:56:50 +0100

    feat(eslint): define recommended and zoneless config

---
+++ libs/eslint-plugin/src/lib/configs/recommended.ts
@@ -0,0 +1,13 @@
+import { TSESLint } from '@typescript-eslint/experimental-utils';
+
+export default {
+  parser: '@typescript-eslint/parser',
+  plugins: ['rx-angular'],
+  rules: {
+    'rx-angular/no-explicit-change-detection-apis': 'error',
+    'rx-angular/no-zone-run-apis': 'warn',
+    'rx-angular/prefer-no-layout-sensitive-apis': 'error',
+    'rx-angular/prefer-no-lodash-clone-deep': 'error',
+    'rx-angular/prefer-no-lodash-is-equal': 'error',
+  },
+} as TSESLint.Linter.Config;
