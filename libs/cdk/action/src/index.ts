9e129e45cee3ef5faa47fadfcc49baa89144e4d2
Michael Hladky <michael@hladky.at>
2021-12-10 22:00:39 +0100

    docs: initial api docs

--- libs/cdk/action/src/index.ts
+++ libs/cdk/action/src/index.ts
@@ -2,17 +2,29 @@ import { map, Observable, Subject } from 'rxjs';
 
 type InstanceOrType<T> = T extends abstract new (...args: unknown[]) => infer R ? R : T;
 
-type UiInteractions<T extends { [x: string]: any }> = {
+export type ActionAccess<T extends { [x: string]: any }> = {
   [K in keyof T]: (arg: InstanceOrType<T[K]>) => void;
 } & {
   [K in Extract<keyof T, string> as `${K}$`]: Observable<T[K]>;
 };
 
-export function getActions<T extends object>(transforms?: Partial<{ [K in keyof T]: (e?: any) => T[K] }>): UiInteractions<T> {
+/**
+ * Returns a object based off of the provided typing with a separate setter `[prop](value: T[K]): void` and observable stream `[prop]$: Observable<T[K]>`;
+ *
+ * @example
+ *
+ * const actions = getActions<search: string, submit: void>({search: (e) => e.target.value});
+ * 
+ * actions.search($event);
+ * actions.search$ | async;
+ *
+ * @param transforms map of transform functions to apply on certain properties if they are set.
+ */
+export function getActions<T extends object>(transforms?: Partial<{ [K in keyof T]: (e?: any) => T[K] }>): ActionAccess<T> {
   transforms = transforms || {};
   const subjects: Subject<T[keyof T]>[] = [];
 
-  const handler: ProxyHandler<UiInteractions<T>> = {
+  const handler: ProxyHandler<ActionAccess<T>> = {
     get(target, property) {
 
       if (property.toString().split('').pop() === '$') {
@@ -31,5 +43,5 @@ export function getActions<T extends object>(transforms?: Partial<{ [K in keyof 
     },
   };
 
-  return new Proxy({} as UiInteractions<T>, handler);
+  return new Proxy({} as ActionAccess<T>, handler);
 }
