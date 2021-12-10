8e8bf6ef4c8883c034fb700e1326f7f71ee24cab
Michael Hladky <michael@hladky.at>
2021-12-10 17:40:12 +0100

    refactor:  implemented updates from @nartc

--- libs/cdk/action/src/index.ts
+++ libs/cdk/action/src/index.ts
@@ -1,36 +1,34 @@
 import { map, Subject } from 'rxjs';
 
-type InstanceOrType<T> = T extends abstract new(...args: unknown[]) => infer R ? R : T;
+type InstanceOrType<T> = T extends abstract new (...args: unknown[]) => infer R
+  ? R
+  : T;
 
-type UiInteractions<T extends { [x: string]: any}> = {
+type UiInteractions<T extends { [x: string]: any }> = {
   [K in keyof T]: (arg: InstanceOrType<T[K]>) => void;
 } & {
-  [K in Extract<keyof T, string> as `${K}$`]: {
-    subscribe(arg: Function): any
-  }
-}
+  [K in Extract<keyof T, string> as `${K}$`]: Observable<T[K]>;
+};
 
-export function getActions<T extends object>(): UiInteractions<T>{
+export function getActions<T extends object>(): UiInteractions<T> {
   const subject = new Subject<Partial<T>>();
 
-  const handler = {
+  const handler: ProxyHandler<UiInteractions<T>> = {
     get(target, property) {
       const propName = property.toString().slice(0, -1);
 
-      if(property.toString().split('').pop() === '$') {
-        return subject.pipe(
-          map(t => t[propName])
-        )
+      if (property.toString().split('').pop() === '$') {
+        return subject.pipe(map((t) => t[propName as keyof T]));
       }
 
-      return (args?) => {
+      return (args: T[keyof T]) => {
         subject.next({ [property]: args } as Partial<T>);
-      }
+      };
     },
     set(target, property, vale) {
       throw new Error('No setters available. To emit call the property name.');
-    }
-  }
+    },
+  };
 
-  return new Proxy({}, handler) as UiInteractions<T>;
+  return new Proxy({} as UiInteractions<T>, handler);
 }
