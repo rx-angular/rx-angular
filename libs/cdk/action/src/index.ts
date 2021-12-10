cad481475c59c1162daed972d0d745d80cb67eb8
Michael Hladky <michael@hladky.at>
2021-12-10 19:05:04 +0100

    refactor: implemented transforms

--- libs/cdk/action/src/index.ts
+++ libs/cdk/action/src/index.ts
@@ -1,8 +1,6 @@
-import { map, Subject } from 'rxjs';
+import { map, Observable, Subject } from 'rxjs';
 
-type InstanceOrType<T> = T extends abstract new (...args: unknown[]) => infer R
-  ? R
-  : T;
+type InstanceOrType<T> = T extends abstract new (...args: unknown[]) => infer R ? R : T;
 
 type UiInteractions<T extends { [x: string]: any }> = {
   [K in keyof T]: (arg: InstanceOrType<T[K]>) => void;
@@ -10,19 +8,22 @@ type UiInteractions<T extends { [x: string]: any }> = {
   [K in Extract<keyof T, string> as `${K}$`]: Observable<T[K]>;
 };
 
-export function getActions<T extends object>(): UiInteractions<T> {
-  const subject = new Subject<Partial<T>>();
+export function getActions<T extends object>(transforms?: Partial<{ [K in keyof T]: (e?: any) => T[K] }>): UiInteractions<T> {
+  transforms = transforms || {};
+  const subjects: Subject<T[keyof T]>[] = [];
 
   const handler: ProxyHandler<UiInteractions<T>> = {
     get(target, property) {
-      const propName = property.toString().slice(0, -1);
 
       if (property.toString().split('').pop() === '$') {
-        return subject.pipe(map((t) => t[propName as keyof T]));
+        const propName = property.toString().slice(0, -1);
+        subjects[propName] =  subjects[propName] ||  new Subject<keyof T>()
+        return subjects[propName].pipe(map((v) => (transforms[propName] ? transforms[propName](v) : v)));
       }
 
       return (args: T[keyof T]) => {
-        subject.next({ [property]: args } as Partial<T>);
+        subjects[property] =  subjects[property] ||  new Subject<keyof T>()
+        subjects[property].next(args);
       };
     },
     set(target, property, vale) {
