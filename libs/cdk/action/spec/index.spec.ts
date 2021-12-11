8a5ea495999052c6c775ed729aed5460be6fa124
Michael Hladky <michael@hladky.at>
2021-12-11 15:49:33 +0100

    test: adopt tests for getActions

--- libs/cdk/action/spec/index.spec.ts
+++ libs/cdk/action/spec/index.spec.ts
@@ -2,21 +2,15 @@ import { mockConsole } from '@test-helpers';
 import { getActions } from '../src';
 import { isObservable } from 'rxjs';
 
-/** @test {coalesceWith} */
+/** @test {getActions} */
 describe('getActions', () => {
   beforeAll(() => mockConsole());
 
-  beforeEach(() => {});
-
-
-  it('should get created properly', (done) => {
-    const values = 'foo';
+  it('should get created properly', () => {
     const actions = getActions<{ prop: string }>();
-    const exp = values;
     expect(typeof actions.prop).toBe('function');
     expect(isObservable(actions.prop)).toBeFalsy();
     expect(isObservable(actions.prop$)).toBeTruthy();
-    done();
   });
 
   it('should emit on the subscribed channels', (done) => {
