8d4c2894e4ee31df33fb75b636028af7270e5075
Michael Hladky <michael@hladky.at>
2021-12-11 15:01:27 +0100

    fix: typing

--- libs/cdk/action/src/index.ts
+++ libs/cdk/action/src/index.ts
@@ -11,6 +11,8 @@ export type ActionAccess<T extends { [x: string]: any }> = {
 /**
  * Returns a object based off of the provided typing with a separate setter `[prop](value: T[K]): void` and observable stream `[prop]$: Observable<T[K]>`;
  *
+ * { search: string } => { search$: Observable<string>, search: (value: string) => void;}
+ *
  * @example
  *
  * const actions = getActions<search: string, submit: void>({search: (e) => e.target.value});
@@ -21,16 +23,16 @@ export type ActionAccess<T extends { [x: string]: any }> = {
  * @param transforms map of transform functions to apply on certain properties if they are set.
  */
 export function getActions<T extends object>(transforms?: Partial<{ [K in keyof T]: (e?: any) => T[K] }>): ActionAccess<T> {
-  transforms = transforms || {};
-  const subjects: Subject<T[keyof T]>[] = [];
+  const _transforms = transforms !== undefined ? transforms : {} as any;
+  const subjects: Record<string, Subject<T[keyof T]>> = {};
 
   const handler: ProxyHandler<ActionAccess<T>> = {
-    get(target, property) {
+    get(_, property: string) {
 
       if (property.toString().split('').pop() === '$') {
-        const propName = property.toString().slice(0, -1);
+        const propName: string | number | Symbol = property.toString().slice(0, -1);
         subjects[propName] =  subjects[propName] ||  new Subject<keyof T>()
-        return subjects[propName].pipe(map((v) => (transforms[propName] ? transforms[propName](v) : v)));
+        return subjects[propName].pipe(map((v) => (_transforms[propName] ? _transforms[propName](v) : v)));
       }
 
       return (args: T[keyof T]) => {
@@ -38,7 +40,7 @@ export function getActions<T extends object>(transforms?: Partial<{ [K in keyof 
         subjects[property].next(args);
       };
     },
-    set(target, property, vale) {
+    set() {
       throw new Error('No setters available. To emit call the property name.');
     },
   };
