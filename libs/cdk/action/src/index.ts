1c602c203a15c31fb2c402b1b930359e6a4598e8
Michael Hladky <michael@hladky.at>
2021-12-13 13:12:45 +0100

    fix: typing of ActionAccess

--- libs/cdk/action/src/index.ts
+++ libs/cdk/action/src/index.ts
@@ -5,8 +5,12 @@ type InstanceOrType<T> = T extends abstract new (...args: unknown[]) => infer R 
 export type ActionAccess<T extends { [x: string]: any }> = {
   [K in keyof T]: (arg: InstanceOrType<T[K]>) => void;
 } & {
-  [K in Extract<keyof T, string> as `${K}$`]: Observable<T[K]>;
+  [K in Extract<keyof T, string> as `${K}$`]: Observable<InstanceOrType<T[K]>>;
 };
+
+export type actionTransformFn<I, O> = (value: I) => O;
+
+export type actionTransforms<T> = { [K in keyof T]: actionTransformFn<unknown, T[K]> };
 
 /**
  * Returns a object based off of the provided typing with a separate setter `[prop](value: T[K]): void` and observable stream `[prop]$: Observable<T[K]>`;
@@ -20,24 +24,26 @@ export type ActionAccess<T extends { [x: string]: any }> = {
  * actions.search($event);
  * actions.search$ | async;
  *
- * @param transforms map of transform functions to apply on certain properties if they are set.
+ * @param transforms map of transform functions to apply on transformations to actions before emitting them.
+ * This is very useful to clean up bloated template. e.g. `[input]="$event?.target?.value"` => `[input]="$event"`
  */
-export function getActions<T extends object>(transforms?: Partial<{ [K in keyof T]: (e?: any) => T[K] }>): ActionAccess<T> {
-  const _transforms = transforms !== undefined ? transforms : {} as any;
-  const subjects: Record<string, Subject<T[keyof T]>> = {};
+export function getActions<T extends object>(transforms?: Partial<actionTransforms<T>>): ActionAccess<T> {
+  const _transforms = transforms !== undefined ? transforms : {} as Partial<actionTransforms<T>>;
+  const subjects = {} as { [K in keyof T]: Subject<T[K]> };
 
   const handler: ProxyHandler<ActionAccess<T>> = {
     get(_, property: string) {
+      const prop = property as keyof T;
 
-      if (property.toString().split('').pop() === '$') {
-        const propName: string | number | Symbol = property.toString().slice(0, -1);
+      if (prop.toString().split('').pop() === '$') {
+        const propName = prop.toString().slice(0, -1) as keyof T;
         subjects[propName] =  subjects[propName] ||  new Subject<T[keyof T]>()
         return subjects[propName].pipe(map((v) => (_transforms[propName] ? _transforms[propName](v) : v)));
       }
 
       return (args: T[keyof T]) => {
-        subjects[property] =  subjects[property] ||  new Subject<T[keyof T]>()
-        subjects[property].next(args);
+        subjects[prop] =  subjects[prop] ||  new Subject<T[keyof T]>()
+        subjects[prop].next(args);
       };
     },
     set() {
