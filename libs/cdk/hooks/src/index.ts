7cbf5f6a9a503fb6eb8a026de3012a3ee34ccea4
Michael Hladky <michael@hladky.at>
2021-12-17 20:00:56 +0100

    refactor: old PoCs

--- libs/cdk/hooks/src/index.ts
+++
@@ -1,8 +0,0 @@
-export {
-  OnDestroy$,
-  DestroyProp,
-  HookProps,
-  SingleShotProps,
-  DestroyChannel$,
-} from '../src/lib/model';
-export { toHook, untilDestroyed, isSingleShotHookNameGuard } from '../src/lib/utils';
