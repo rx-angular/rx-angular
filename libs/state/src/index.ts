b7423a58cd3b475fa0cbfa0af5d4b2c31793d127
Julian Jandl <julian@jandl.wien>
2021-12-18 16:59:58 +0100

    refactor: re-introduce deprecation layer + re-export from state/selections

--- libs/state/src/index.ts
+++ libs/state/src/index.ts
@@ -5,3 +5,6 @@ export {
   ProjectStateReducer,
   ProjectValueReducer,
 } from './lib/rx-state.service';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+export * from '@rx-angular/state/selections';
+export * from './lib/deprecated/transformation-helpers';
