f674a5099a7005dc4390730c0cfd3b8cb222ad92
Julian Jandl <julian@jandl.wien>
2022-02-06 23:55:50 +0100

    refactor(demos): use actual rxFor directive instead of PoC version

--- apps/demos/src/app/features/template/rx-for/error-handling/rx-for-error-handling.module.ts
+++ apps/demos/src/app/features/template/rx-for/error-handling/rx-for-error-handling.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { RxForModule } from '../../../../rx-angular-pocs/template/directives/for/rx-for.module';
+import { ForModule } from '@rx-angular/template/experimental/for';
 import { StrategySelectModule } from '../../../../shared/debug-helper/strategy-select/strategy-select.module';
 import { ValueProvidersModule } from '../../../../shared/debug-helper/value-provider/value-providers.module';
 import { ErrorHandlingParentComponent } from './error-handling-parent.component';
 import { ErrorHandlingChildComponent } from './error-handling-child.component';
 
-
 @NgModule({
   declarations: [ErrorHandlingParentComponent, ErrorHandlingChildComponent],
   imports: [
     CommonModule,
-    RxForModule,
+    ForModule,
     RouterModule.forChild([
       {
         path: '',
