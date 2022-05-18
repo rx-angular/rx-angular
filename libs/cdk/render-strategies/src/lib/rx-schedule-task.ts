e1b8629703b07d6c021d59359062d6a183901441
Kirill Karnaukhov <karnaukhov.kh@gmail.com>
2022-05-18 20:12:46 +0300

    fix(cdk): fix rxScheduleTask imports

--- libs/cdk/render-strategies/src/lib/rx-schedule-task.ts
+++ libs/cdk/render-strategies/src/lib/rx-schedule-task.ts
@@ -1,9 +1,9 @@
+import { NgZone } from '@angular/core';
 import {
   cancelCallback,
   scheduleCallback,
-} from '../../../internals/scheduler/src/lib/scheduler';
-import { SchedulerTaskZone } from '../../../internals/scheduler/src/lib/schedulerMinHeap';
-import { PriorityLevel } from '../../../internals/scheduler/src/lib/schedulerPriorities';
+  PriorityLevel,
+} from '@rx-angular/cdk/internals/scheduler';
 import { RxConcurrentStrategyNames } from './model';
 
 type StrategiesPriorityRecord = Record<
@@ -33,7 +33,7 @@ export const rxScheduleTask = (
   strategy: keyof StrategiesPriorityRecord = 'normal',
   options?: {
     delay?: number;
-    ngZone?: SchedulerTaskZone;
+    ngZone?: NgZone;
   }
 ) => {
   const task = scheduleCallback(
