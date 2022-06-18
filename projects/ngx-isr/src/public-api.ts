cd1aee277ec9da8c077e08be02976fb4b30ca4f4
Enea Jahollari <jahollarienea14@gmail.com>
2022-06-18 19:18:06 +0200

    feat: make FileSystemCacheHandler public

--- projects/ngx-isr/src/public-api.ts
+++ projects/ngx-isr/src/public-api.ts
@@ -5,7 +5,7 @@
 export * from './lib/ngx-isr.service';
 export * from './lib/isr-handler';
 
-export { InMemoryCacheHandler } from './lib/cache-handlers';
+export { InMemoryCacheHandler, FileSystemCacheHandler } from './lib/cache-handlers';
 
 export { NgxIsrModule } from './lib/ngx-isr.module';
 
