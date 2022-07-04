1538971ad743d360d234e348ae4247800f4a3655
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-07-04 23:35:11 +0200

    docs: remove custom logo style

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <img src="img/logo.png" className={styles.logo} />
+        <img src="img/logo.png" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
