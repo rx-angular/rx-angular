768ebea79d4b5885ca121329fba2b27ed74d6983
Julian Jandl <julian@jandl.wien>
2022-11-07 22:09:39 +0100

    feat(template): rxIfs viewContext should only allow boolean values

--- libs/template/experimental/if/src/lib/if.directive.ts
+++ libs/template/experimental/if/src/lib/if.directive.ts
@@ -1,7 +1,6 @@
 import {
   ChangeDetectorRef,
   Directive,
-  EmbeddedViewRef,
   Input,
   NgZone,
   OnChanges,
@@ -11,12 +10,15 @@ import {
   TemplateRef,
   ViewContainerRef,
 } from '@angular/core';
-import { coerceAllFactory, coerceObservable } from '@rx-angular/cdk/coercing';
+import { coerceAllFactory } from '@rx-angular/cdk/coercing';
 import {
   createTemplateNotifier,
   RxNotificationKind,
 } from '@rx-angular/cdk/notifications';
-import { RxStrategyProvider } from '@rx-angular/cdk/render-strategies';
+import {
+  RxStrategyNames,
+  RxStrategyProvider,
+} from '@rx-angular/cdk/render-strategies';
 import {
   createTemplateManager,
   RxTemplateManager,
@@ -24,6 +26,7 @@ import {
 import {
   NextObserver,
   Observable,
+  ObservableInput,
   ReplaySubject,
   Subject,
   Subscription,
@@ -35,52 +38,311 @@ import {
   RxIfViewContext,
 } from './model/index';
 
+/**
+ * @Directive IfDirective
+ * @description
+ *
+ * The `RxIf` directive is drop-in replacement for the `NgIf` directive, but with additional features.
+ * `RxIf` allows you to bind observables directly without having the need of using the `async`
+ * pipe in addition.
+ *
+ * This enables `rxIf` to completely operate on its own without having to interact with `NgZone`
+ * or triggering global change detection.
+ *
+ * Read more about the RxIf directive in the [official docs](https://www.rx-angular.io/docs/template/api/rx-if-directive).
+ *
+ * @example
+ * <app-item *rxIf="show$"></app-item>
+ *
+ * @docsCategory RxIf
+ * @docsPage RxIf
+ * @publicApi
+ */
 @Directive({
   selector: '[rxIf]',
 })
 // eslint-disable-next-line @angular-eslint/directive-class-suffix
-export class RxIf<U> implements OnInit, OnChanges, OnDestroy, OnChanges {
+export class RxIf implements OnInit, OnChanges, OnDestroy, OnChanges {
+  /** @internal */
   private subscription = new Subscription();
-  private _renderObserver: NextObserver<any>;
+  /** @internal */
+  private _renderObserver: NextObserver<unknown>;
+  /** @internal */
   private templateManager: RxTemplateManager<
-    U,
-    RxIfViewContext<U>,
+    boolean,
+    RxIfViewContext,
     rxIfTemplateNames
   >;
 
-  private lastValidValue: U | null;
-  @Input() rxIf: Observable<U> | U | null | undefined;
+  /** @internal */
+  private lastValidValue: boolean | null;
 
+  /**
+   * @description
+   * The Observable or value to representing the condition.
+   *
+   * @example
+   * showHero = true;
+   * showHero$ = new BehaviorSubject<boolean>(true);
+   *
+   * <ng-container *rxIf="showHero">
+   *   <app-hero></app-hero>
+   * </ng-container>
+   *
+   * <ng-container *rxIf="showHero$">
+   *   <app-hero></app-hero>
+   * </ng-container>
+   *
+   * @param { ObservableInput<boolean> | boolean } rxIf
+   */
+  @Input() rxIf: ObservableInput<boolean> | boolean;
+
+  /**
+   * @description
+   *
+   * You can change the used `RenderStrategy` by using the `strategy` input of the `*rxIf`. It accepts
+   * an `Observable<RxStrategyNames>` or
+   *   [`RxStrategyNames`](https://github.com/rx-angular/rx-angular/blob/b0630f69017cc1871d093e976006066d5f2005b9/libs/cdk/render-strategies/src/lib/model.ts#L52).
+   *
+   * The default value for strategy is
+   * [`normal`](https://www.rx-angular.io/docs/template/cdk/render-strategies/strategies/concurrent-strategies).
+   *
+   * Read more about this in the
+   * [official docs](https://www.rx-angular.io/docs/template/api/let-directive#use-render-strategies-strategy).
+   *
+   * @example
+   *
+   * \@Component({
+   *   selector: 'app-root',
+   *   template: `
+   *     <ng-container *rxIf="showHero$; strategy: 'userBlocking'">
+   *       <app-hero></app-hero>
+   *     </ng-container>
+   *
+   *     <ng-container *rxIf="showHero$; strategy: strategy$">
+   *       <app-hero></app-hero>
+   *     </ng-container>
+   *   `
+   * })
+   * export class AppComponent {
+   *   strategy$ = of('immediate');
+   * }
+   *
+   * @param { string | Observable<string> | undefined } strategyName
+   * @see {@link RxStrategyNames}
+   */
   @Input('rxIfStrategy')
-  set strategy(strategyName: Observable<string> | string | null | undefined) {
+  set strategy(
+    strategyName: Observable<RxStrategyNames<string>> | RxStrategyNames<string>
+  ) {
     this.strategyHandler.next(strategyName);
   }
 
-  @Input('rxIfElse') else: TemplateRef<RxIfViewContext<U>>;
-  @Input('rxIfThen') then: TemplateRef<RxIfViewContext<U>>;
+  /**
+   * @description
+   * Defines the template to be used when the bound value is falsy
+   *
+   * @example
+   * <app-hero *rxIf="show$; else: noHero"></app-hero>
+   * <ng-template #noHero><no-hero></no-hero></ng-template>
+   */
+  @Input('rxIfElse') else: TemplateRef<RxIfViewContext>;
 
-  @Input('rxIfSuspense') suspense: TemplateRef<RxIfViewContext<U>>;
-  @Input('rxIfComplete') complete: TemplateRef<RxIfViewContext<U>>;
-  @Input('rxIfError') error: TemplateRef<RxIfViewContext<U>>;
+  /**
+   * @description
+   * Defines the template to be used when the bound value is truthy
+   *
+   * @example
+   * <ng-container *rxIf="show$; then: hero"></ng-container>
+   * <ng-template #hero><app-hero></app-hero></ng-template>
+   */
+  @Input('rxIfThen') then: TemplateRef<RxIfViewContext>;
 
+  /**
+   * @description
+   * Defines the template for the suspense state. Will be
+   * shown when the bound Observable is in "suspense" state.
+   * Suspense state is active when the current value is undefined or no value
+   * was ever emitted.
+   *
+   * Read more about the reactive context in the
+   * [official docs](https://www.rx-angular.io/docs/template/concepts/reactive-context).
+   *
+   * @example
+   * <app-hero *rxIf="show$; suspense: suspenseTemplate" ></app-hero>
+   * <ng-template #suspenseTemplate>
+   *   <mat-progress-spinner></mat-progress-spinner>
+   * </ng-template>
+   *
+   * @param { TemplateRef<RxIfViewContext> } suspense
+   */
+  @Input('rxIfSuspense') suspense: TemplateRef<RxIfViewContext>;
+
+  /**
+   * @description
+   * Defines the template for the complete state. Will be
+   * shown when the bound Observable is in "complete" state.
+   *
+   * Read more about the reactive context in the
+   * [official docs](https://www.rx-angular.io/docs/template/concepts/reactive-context).
+   *
+   * @example
+   * <app-hero *rxIf="show$; complete: completeTemplate" ></app-hero>
+   * <ng-template #completeTemplate>
+   *   <icon>thumbs_up</icon>
+   * </ng-template>
+   *
+   * @param { TemplateRef<RxIfViewContext> } suspense
+   */
+  @Input('rxIfComplete') complete: TemplateRef<RxIfViewContext>;
+
+  /**
+   * @description
+   * Defines the template for the error state. Will be
+   * shown when the bound Observable is in "error" state.
+   *
+   * Read more about the reactive context in the
+   * [official docs](https://www.rx-angular.io/docs/template/concepts/reactive-context).
+   *
+   * @example
+   * <app-hero *rxIf="show$; error: errorTemplate" ></app-hero>
+   * <ng-template #errorTemplate>
+   *   <icon>error</icon>
+   * </ng-template>
+   *
+   * @param { TemplateRef<RxIfViewContext> } suspense
+   */
+  @Input('rxIfError') error: TemplateRef<RxIfViewContext>;
+
+  /**
+   * @description
+   *
+   * Structural directives maintain `EmbeddedView`s within a components' template.
+   * Depending on the bound value as well as the configured `RxRenderStrategy`,
+   * updates processed by the `*rxIf` directive might be asynchronous.
+   *
+   * Whenever a template gets inserted into, or removed from, its parent component, the directive has to inform the parent in order to
+   * update any view- or contentquery (`@ViewChild`, `@ViewChildren`, `@ContentChild`, `@ContentChildren`).
+   *
+   * Read more about this in the
+   * [official docs](https://www.rx-angular.io/docs/template/api/rx-if-directive#local-strategies-and-view-content-queries-parent).
+   *
+   * @example
+   * \@Component({
+   *   selector: 'app-root',
+   *   template: `
+   *    <app-component>
+   *      <app-item
+   *        *rxIf="
+   *          show$;
+   *          parent: true;
+   *        "
+   *      >
+   *      </app-item>
+   *    </app-component>
+   *   `
+   * })
+   * export class AppComponent {
+   *   show$ = state.select('showItem');
+   * }
+   *
+   * @param {boolean} renderParent
+   */
   @Input('rxIfParent') renderParent = this.strategyProvider.config.parent;
 
+  /**
+   * @description
+   * A flag to control whether `*rxIf` templates are created within `NgZone` or not.
+   * The default value is `true, `*rxIf` will create its `EmbeddedView` inside `NgZone`.
+   *
+   * Event listeners normally trigger zone.
+   * Especially high frequency events can cause performance issues.
+   *
+   * Read more about this in the
+   * [official docs](https://www.rx-angular.io/docs/template/api/let-directive#working-with-event-listeners-patchzone).
+   *
+   * @example
+   * \@Component({
+   *   selector: 'app-root',
+   *   template: `
+   *    <app-component>
+   *      <app-item
+   *        *rxIf="
+   *          show$;
+   *          patchZone: false;
+   *        "
+   *        (drag)="itemDrag($event)"
+   *      >
+   *      </app-item>
+   *    </app-component>
+   *   `
+   * })
+   * export class AppComponent {
+   *   show$ = state.select('showItem');
+   * }
+   *
+   * @param {boolean} patchZone
+   */
   @Input('rxIfPatchZone') patchZone = this.strategyProvider.config.patchZone;
 
+  /**
+   * @description
+   * A `Subject` which emits whenever `*rxIf` rendered a change to the view.
+   * This enables developers to perform actions when rendering has been done.
+   * The `renderCallback` is useful in situations where you
+   * rely on specific DOM properties like the dimensions of an item after it got rendered.
+   *
+   * The `renderCallback` emits the latest value causing the view to update.
+   *
+   * @example
+   * \@Component({
+   *   selector: 'app-root',
+   *   template: `
+   *    <app-component>
+   *      <app-item
+   *        *rxIf="
+   *          show$;
+   *          renderCallback: rendered;
+   *        "
+   *      >
+   *      </app-item>
+   *    </app-component>
+   *   `
+   * })
+   * export class AppComponent {
+   *  show$ = state.select('showItem');
+   *  // this emits whenever rxIf finished rendering changes
+   *  rendered = new Subject<boolean>();
+   *
+   *   constructor(elementRef: ElementRef<HTMLElement>) {
+   *     rendered.subscribe(() => {
+   *       // item is rendered, we can access its dom now
+   *     })
+   *   }
+   * }
+   *
+   * @param {Subject<boolean>} callback
+   */
   @Input('rxIfRenderCallback')
-  set renderCallback(callback: NextObserver<U>) {
+  set renderCallback(callback: NextObserver<boolean>) {
     this._renderObserver = callback;
   }
 
   /** @internal */
-  private observablesHandler = createTemplateNotifier<U>();
-  private readonly strategyHandler = coerceAllFactory<string>(
-    () => new ReplaySubject<string | Observable<string>>(1),
+  private observablesHandler = createTemplateNotifier<boolean>();
+
+  /** @internal */
+  private readonly strategyHandler = coerceAllFactory<RxStrategyNames<string>>(
+    () =>
+      new ReplaySubject<
+        RxStrategyNames<string> | Observable<RxStrategyNames<string>>
+      >(1),
     mergeAll()
   );
+  /** @internal */
   private readonly rendered$ = new Subject<void>();
-
-  private get thenTemplate(): TemplateRef<RxIfViewContext<U>> {
+  /** @internal */
+  private get thenTemplate(): TemplateRef<RxIfViewContext> {
     return this.then ? this.then : this.templateRef;
   }
 
@@ -88,10 +350,11 @@ export class RxIf<U> implements OnInit, OnChanges, OnDestroy, OnChanges {
     private strategyProvider: RxStrategyProvider,
     private cdRef: ChangeDetectorRef,
     private ngZone: NgZone,
-    private readonly templateRef: TemplateRef<RxIfViewContext<U>>,
+    private readonly templateRef: TemplateRef<RxIfViewContext>,
     private readonly viewContainerRef: ViewContainerRef
   ) {}
 
+  /** @internal */
   ngOnInit() {
     this.subscription.add(
       this.observablesHandler.values$.subscribe(({ kind, value }) => {
@@ -139,6 +402,7 @@ export class RxIf<U> implements OnInit, OnChanges, OnDestroy, OnChanges {
         RxIfTemplateNames.suspense,
         this.suspense
       );
+      this.observablesHandler.withInitialSuspense(!!this.suspense);
     }
 
     if (changes.error) {
@@ -146,16 +410,16 @@ export class RxIf<U> implements OnInit, OnChanges, OnDestroy, OnChanges {
     }
 
     if (changes.rxIf) {
-      this.observablesHandler.next(
-        this.rxIf === undefined ? this.rxIf : coerceObservable(this.rxIf)
-      );
+      this.observablesHandler.next(this.rxIf);
     }
   }
 
+  /** @internal */
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
+  /** @internal */
   private _createTemplateManager(): void {
     const getNextTemplate = (value) => {
       return value
@@ -165,8 +429,8 @@ export class RxIf<U> implements OnInit, OnChanges, OnDestroy, OnChanges {
         : undefined;
     };
     this.templateManager = createTemplateManager<
-      U,
-      RxIfViewContext<U>,
+      boolean,
+      RxIfViewContext,
       rxIfTemplateNames
     >({
       templateSettings: {
@@ -204,29 +468,12 @@ export class RxIf<U> implements OnInit, OnChanges, OnDestroy, OnChanges {
   }
 }
 
-function createViewContext<T>(value: T): RxIfViewContext<T> {
-  return {
-    rxIf: value,
-    $implicit: value,
-    error: false,
-    complete: false,
-    suspense: false,
-  };
-}
-
-function updateViewContext<T>(
-  value: T,
-  view: EmbeddedViewRef<RxIfViewContext<T>>,
-  context: RxIfViewContext<T>
-): void {
-  Object.keys(context).forEach((k) => {
-    view.context[k] = context[k];
-  });
-}
-
 /**
+ * @internal
+ * @description
  * Coerces a data-bound value (typically a string) to a boolean.
+ *
  */
-function coerceBooleanProperty(value: any): boolean {
+function coerceBooleanProperty(value: unknown): boolean {
   return value != null && `${value}` !== 'false';
 }
--- libs/template/experimental/if/src/lib/model/view-context.ts
+++ libs/template/experimental/if/src/lib/model/view-context.ts
@@ -1,7 +1,6 @@
 import { RxViewContext } from '@rx-angular/cdk/template';
 
-export interface RxIfViewContext<T> extends RxViewContext<T> {
+export interface RxIfViewContext extends RxViewContext<boolean> {
   // to enable `as` syntax we have to assign the directives selector (var as v)
-  rxIf: T | undefined;
-  rxElse?: boolean;
+  rxIf: boolean;
 }
