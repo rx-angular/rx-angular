125dbcfc1ca0b3d16b58cb9c2b25b6198c624893
Edouard Bozon <bozonedouard@gmail.com>
2022-11-23 09:17:53 +0100

    refactor(template): remove root barrel exports
    BREAKING CHANGE: Modules and directives are now removed from the root entrypoint `'@rx-angular/template'`.
    
    Instead, use secondary entrypoint to import the symbol, for instance: `import { LetModule } from '@rx-angular/template/let';`.

--- libs/template/src/index.ts
+++ libs/template/src/index.ts
@@ -1,14 +1,1 @@
-// EXPERIMENTAL
-// TODO(artur): we're keeping it here until we get a migration script.
-export {
-  UnpatchModule as UnpatchEventsModule,
-  UnpatchDirective,
-} from '@rx-angular/template/unpatch';
-
-// STABLE
-// TODO(artur): we're keeping it here until we get a migration script.
-export { LetModule, LetDirective } from '@rx-angular/template/let';
-
-export { PushModule, PushPipe } from '@rx-angular/template/push';
-
-export { ForModule, RxFor, RxForViewContext } from '@rx-angular/template/for';
+export {};
