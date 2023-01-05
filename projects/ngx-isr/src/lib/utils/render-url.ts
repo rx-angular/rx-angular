4e056deec91ee686bbe23517ff2bc858323c56cc
Enea Jahollari <jahollarienea14@gmail.com>
2023-01-05 18:26:51 +0100

    feat: added server context provider

--- projects/ngx-isr/src/lib/utils/render-url.ts
+++ projects/ngx-isr/src/lib/utils/render-url.ts
@@ -1,5 +1,6 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { Provider } from '@angular/core';
+import { ɵSERVER_CONTEXT as SERVER_CONTEXT } from '@angular/platform-server';
 
 export interface RenderUrlConfig {
   req: any;
@@ -9,6 +10,10 @@ export interface RenderUrlConfig {
   providers?: Provider[];
 }
 
+const EXTRA_PROVIDERS: Provider[] = [
+  { provide: SERVER_CONTEXT, useValue: 'ngx-isr' },
+];
+
 // helper method that generates html of an url
 export const renderUrl = async (options: RenderUrlConfig): Promise<string> => {
   const { req, res, url, indexHtml, providers } = options;
@@ -17,13 +22,19 @@ export const renderUrl = async (options: RenderUrlConfig): Promise<string> => {
   req.url = url;
   req.originalUrl = url;
 
+  const BASE_URL_PROVIDER: Provider = {
+    provide: APP_BASE_HREF,
+    useValue: req.baseUrl,
+  };
+
   return new Promise((resolve, reject) => {
+    const allProviders = providers
+      ? [...providers, ...EXTRA_PROVIDERS] // if providers are provided, we add them to the list
+      : [...EXTRA_PROVIDERS, BASE_URL_PROVIDER]; // if not, we add the default providers
+
     res.render(
       indexHtml,
-      {
-        req,
-        providers: providers ?? [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
-      },
+      { req, providers: allProviders },
       async (err: Error, html: string) => {
         if (err) {
           reject(err);
