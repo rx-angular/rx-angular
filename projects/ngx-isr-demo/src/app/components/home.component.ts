2734d7ea2df1814ef8fbc22c7eef397e14de9533
Enea Jahollari <jahollarienea14@gmail.com>
2023-01-06 01:12:46 +0100

    update docs

--- projects/ngx-isr-demo/src/app/components/home.component.ts
+++ projects/ngx-isr-demo/src/app/components/home.component.ts
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
   template: `
     <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-            <a href="#" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+            <a href="https://github.com/eneajaho/ngx-isr/releases/tag/v.0.4.0" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
                 <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">
-                  v0.3.4 is out! See what's new
+                  v0.4.0 is out! See what's new
                 </span> 
                 <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
             </a>
@@ -29,7 +29,7 @@ import { Component } from '@angular/core';
                     Watch video
                 </a>  
             </div>
-            <div class="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+            <!-- <div class="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
                 <span class="font-semibold text-gray-400 uppercase">FEATURED IN</span>
                 <div class="flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
                     <a href="#" class="mr-5 mb-5 lg:mb-0 hover:text-gray-800 dark:hover:text-gray-400">
@@ -61,7 +61,7 @@ import { Component } from '@angular/core';
                         </svg>                                                   
                     </a>         
                 </div>
-            </div> 
+            </div>  -->
         </div>
     </section>
   `,
