14e1732a1e40b641ff9dc1a7a0c3c0350d169676
Julian Jandl <julian@jandl.wien>
2023-04-05 12:47:34 +0200

    feat(demos): implement signal store like service

--- apps/demos/src/app/features/state/signal-state/signal-state.component.ts
+++ apps/demos/src/app/features/state/signal-state/signal-state.component.ts
@@ -1,13 +1,17 @@
+import { NgForOf } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   effect,
   inject,
+  Injectable,
   OnInit,
   signal,
 } from '@angular/core';
-import { insert } from '@rx-angular/cdk/transformations';
+import { insert, remove, update } from '@rx-angular/cdk/transformations';
 import { RxState } from '@rx-angular/state';
+import { RxActionFactory } from '@rx-angular/state/actions';
+import { Observable } from 'rxjs';
 
 type Todo = {
   id: number;
@@ -15,63 +19,138 @@ type Todo = {
   done: boolean;
 };
 
+abstract class TodoService {
+  abstract loadTodos(): Observable<Todo[]>;
+  abstract updateTodo(todo: Todo): Observable<Todo>;
+
+  abstract createTodo(todo: Todo): Observable<Todo>;
+
+  abstract deleteTodo(todo: Todo): Observable<void>;
+}
+
 const todoData: Todo[] = new Array(20).fill(null).map((v, i) => ({
   id: i,
   title: `The todo #${i + 1}`,
   done: false,
 }));
 
-const todos = signal(todoData);
+const todosDataSignal = signal(todoData);
+
+/*function describeState<
+  T extends object,
+  M extends Record<string, (args: any) => Partial<T>> = {}
+>(description: {
+  withState?: Partial<T>;
+  withMutations: M;
+}): RxState<T> & {
+  mutations: {
+    [K in keyof M]: (args: Parameters<M[K]>[0]) => void;
+  };
+} {
+  const state: RxState<T> = new RxState();
+  if (description.withState) {
+    state.set(description.withState);
+  }
+
+  // @ts-ignore
+  return state;
+}
+
+const test = describeState({
+  withState: <{ query: string; todos: Todo[] }>{ query: '', todos: [] },
+  withMutations: {
+    addTodo: (todo: Todo) => ({ todos: [] /!*insert(state.todos, todo)*!/ }),
+  },
+});
+test.mutations.addTodo({ id: 2, title: 'test', done: false });*/
+
+interface TodoState {
+  todos: Todo[];
+  query: string;
+}
+
+@Injectable()
+class TodoStore extends RxState<TodoState> {
+  // computations
+  readonly todos = this.signal('todos');
+  readonly filteredTodos = this.computed(({ todos, query }) =>
+    query()
+      ? todos().filter(({ title }) =>
+          title.toLowerCase().includes(query().toLowerCase())
+        )
+      : todos()
+  );
+  constructor() {
+    super();
+    this.set({ todos: [], query: '' });
+    this.loadTodos();
+  }
+  // effects
+  loadTodos() {
+    this.connect('todos', todosDataSignal);
+  }
+
+  // mutations
+  filter(query: string) {
+    this.set({ query });
+  }
+  toggleDone(todo: Todo) {
+    this.set('todos', ({ todos }) =>
+      update(todos, { ...todo, done: !todo.done }, 'id')
+    );
+  }
+  addTodo(todo: Todo) {
+    this.set('todos', ({ todos }) => insert(todos, todo));
+  }
+  removeTodo(todo: Todo) {
+    this.set('todos', ({ todos }) => remove(todos, todo, 'id'));
+  }
+}
 
 @Component({
   selector: 'signal-state',
-  template: ` <div>Signal State</div> `,
+  template: `
+    <h2>Signal State</h2>
+    <div class="d-flex">
+      <input #input (input)="store.filter(input.value)" />
+      <button
+        (click)="
+          store.addTodo({
+            id: store.todos().length + 1,
+            title: 'added',
+            done: false
+          })
+        "
+      >
+        Add
+      </button>
+    </div>
+    <div class="d-flex flex-wrap todo-list">
+      <div class="todo" *ngFor="let todo of store.filteredTodos()">
+        <div>#{{ todo.id }}</div>
+        <div>{{ todo.title }}</div>
+        <button (click)="store.toggleDone(todo)">Done: {{ todo.done }}</button>
+        <button (click)="store.removeTodo(todo)">remove</button>
+      </div>
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  providers: [RxState],
+  providers: [TodoStore],
+  imports: [NgForOf],
+  styles: [
+    `
+      .todo-list {
+        gap: 0.5em;
+      }
+      .todo {
+        border: 1px solid gray;
+      }
+    `,
+  ],
 })
-export class SignalStateComponent implements OnInit {
-  private readonly state = inject<
-    RxState<{ todos: Todo[]; loading: boolean; foo: string }>
-  >(RxState, {
+export class SignalStateComponent {
+  readonly store = inject(TodoStore, {
     self: true,
   });
-
-  ngOnInit() {
-    // connect a signal:
-    this.state.connect('todos', todos);
-    // get a signal:
-    const todosSignal = this.state.signal('todos');
-    // get a computed:
-    const count = this.state.computed(({ todos }) => {
-      return todos().length;
-    });
-
-    // mutate the state
-    setTimeout(() => {
-      this.state.set('todos', ({ todos }) =>
-        insert(todos, { id: 23, done: false, title: 'somenewtitle' })
-      );
-    }, 100);
-
-    // create an effect from plain signal
-    effect(() => {
-      console.log('from plain signal');
-      console.log(todosSignal());
-    });
-
-    // create an effect from computed
-    effect(() => {
-      console.log('from computed');
-      console.log(count());
-    });
-
-    // create an effect from state signals
-    this.state.effect(({ todos }) => {
-      console.log('from state effect');
-      if (todos().length > 20) {
-        console.log('something was added');
-      }
-    });
-  }
 }
