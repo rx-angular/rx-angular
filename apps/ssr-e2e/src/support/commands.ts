8d9e9888ada5928550168b974b4bea7e916d7ead
Edouard Bozon <bozonedouard@gmail.com>
2023-04-12 16:01:15 +0200

    chore: cleanup commands

--- apps/ssr-e2e/src/support/commands.ts
+++ apps/ssr-e2e/src/support/commands.ts
@@ -11,23 +11,5 @@
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface Chainable<Subject> {
-    login(email: string, password: string): void;
-  }
+  interface Chainable<Subject> {}
 }
-//
-// -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
-});
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
