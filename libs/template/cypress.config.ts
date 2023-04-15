8698e1e95b5bba7a52fb0592397d0f902be16c64
Julian Jandl <julian@jandl.wien>
2023-04-15 02:34:16 +0200

    test(template): setup cypress component tests

---
+++ libs/template/cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress';
+import { nxComponentTestingPreset } from '@nrwl/angular/plugins/component-testing';
+
+export default defineConfig({
+  component: {
+    ...nxComponentTestingPreset(__filename),
+    specPattern: ['**/*.cy.ts'],
+  },
+  viewportHeight: 800,
+});
