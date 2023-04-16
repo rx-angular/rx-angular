644db89929c4a468812c86b4433e3b1a98f23c97
Enea Jahollari <jahollarienea14@gmail.com>
2023-04-16 22:14:17 +0200

    docs: cache doc pages using isr

--- projects/docs/src/app/docs/routes.ts
+++ projects/docs/src/app/docs/routes.ts
@@ -9,38 +9,47 @@ const routes: Routes = [
   {
     path: 'intro',
     loadComponent: () => import('./pages/intro.component'),
+    data: { revalidate: 0 },
   },
   {
     path: 'getting-started',
     loadComponent: () => import('./pages/getting-started.component'),
+    data: { revalidate: 0 },
   },
   {
     path: 'how-it-works',
     loadComponent: () => import('./pages/how-it-works.component'),
+    data: { revalidate: 0 },
   },
   {
     path: 'error-handling',
     loadComponent: () => import('./pages/error-handling.component'),
+    data: { revalidate: 0 },
   },
   {
     path: 'on-demand-revalidation',
     loadComponent: () => import('./pages/on-demand-revalidation.component'),
+    data: { revalidate: 0 },
   },
   {
     path: 'cache-handlers',
     loadComponent: () => import('./pages/cache-handlers.component'),
+    data: { revalidate: 0 },
   },
   {
     path: 'pre-rendering-and-isr',
     loadComponent: () => import('./pages/prerendering-isr.component'),
+    data: { revalidate: 0 },
   },
   {
     path: 'create-your-own-cache-handler',
     loadComponent: () => import('./pages/create-own-cache-handler.component'),
+    data: { revalidate: 0 },
   },
   {
     path: 'modify-html-cache-hooks',
     loadComponent: () => import('./pages/modify-html-hooks.component'),
+    data: { revalidate: 0 },
   },
   {
     path: 'use-ngx-isr-service-for-more-than-caching',
@@ -49,10 +58,12 @@ const routes: Routes = [
   {
     path: 'api',
     loadComponent: () => import('./pages/api.component'),
+    data: { revalidate: 0 },
   },
   {
     path: '**',
     loadComponent: () => import('./pages/not-found.component'),
+    data: { revalidate: 0 },
   }
 ];
 
