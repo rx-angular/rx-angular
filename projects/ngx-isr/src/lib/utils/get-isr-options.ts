3fedcc178b6009880e2662f3469c4537d307bde7
Enea Jahollari <jahollarienea14@gmail.com>
2023-04-17 19:28:49 +0200

    chore: add code documentation for getRouteISRDataFromHTML helper fn

--- projects/ngx-isr/src/lib/utils/get-isr-options.ts
+++ projects/ngx-isr/src/lib/utils/get-isr-options.ts
@@ -1,23 +1,41 @@
-import { ISROptions } from '../models';
-
-// this script tag will be included in the page if one of the routes on the page
-// has `revalidate` key in its route data
-const isrScriptTag = '<script id="isr-state" type="application/json">';
-
-export function getISROptions(html: string): ISROptions {
-  const indexOfScriptTag = html?.indexOf(isrScriptTag);
+/**
+ * Get ISR options from html string
+ * @param html
+ * @returns
+ * @example
+ * ```typescript
+ * const { revalidate, errors } = getISROptions(html);
+ * ```
+ * @internal
+ */
+export function getRouteISRDataFromHTML(html: string): {
+  revalidate: number | null;
+  errors: string[];
+} {
+  const indexOfScriptTag = html?.indexOf(ISR_SCRIPT_TAG);
 
   // check if script tag is not included
   if (!html || indexOfScriptTag === -1) {
     return { revalidate: null, errors: [] };
   }
 
-  const isrScript = html.substring(indexOfScriptTag); // start from script till the end of html file
-  const indexOfCloseScriptTag = isrScript.indexOf("</script>"); // first occurrence of closing script tag
+  // start from script till the end of html file
+  const isrScript = html.substring(indexOfScriptTag);
+
+  // first occurrence of closing script tag
+  const indexOfCloseScriptTag = isrScript.indexOf('</script>');
 
   const val = isrScript
     .substring(0, indexOfCloseScriptTag) // remove close script tag
-    .replace(isrScriptTag, "") // remove start script tag
+    .replace(ISR_SCRIPT_TAG, ''); // remove start script tag
 
   return JSON.parse(val);
 }
+
+/**
+ * Script tag that will be included in the page if one of the routes on the page
+ * has `revalidate` key in its route data
+ *
+ * @internal
+ */
+const ISR_SCRIPT_TAG = '<script id="isr-state" type="application/json">';
