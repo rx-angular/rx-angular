786f87c24858b4cac00bb52039f48ed05588cf1d
Julian Jandl <julian@jandl.wien>
2023-04-21 19:08:49 +0200

    feat(template): introduce experimental virtual-scrolling sub-package (#1539)
    * feat(template): introduce experimental virtual-scrolling package
    
    * docs: add docs for virtual-scrolling package
    
    * feat(demos): add virtual scrolling demo application
    
    * chore: cleanup docs
    
    * fix(template): rxVirtualFor: fix static and cleanup api
    
    * refactor(template): virtual-scrolling: simplify code base
    
    * fix(template): rxVirtualFor: react to mutable input changes
    
    * refactor(template): cleanup virtual-scrolling interfaces
    
    * fix(template): dynamicSize properly calculates scrolledIndex & range
    
    * refactor(demos): improve virtual scrolling demo
    
    * refactor(template): remove iterable differ from dynamicSize
    
    * perf(template): replace iterable differ for autosize
    
    * refactor(template): make sync scrollbar styles configurable
    
    * refactor(template): remove viewRange from VirtualScrollViewport
    
    * refactor(template): autosize listens to resize events earlier
    
    * refactor(demos): adjust demo app
    
    * refactor(template): improve autosize diffing
    
    * refactor(template): improve autosize viewport caching
    
    * test(template): implement component-tests for experimental virtual-scrolling (#1542)
    
    * test(template): setup cypress component tests
    
    * test(template): implement first test for virtual-scrolling
    
    * test(template): implement tests for DynamicSizeVirtualScrollStrategy
    
    * test(template): implement tests for AutosizeVirtualScrollStrategy
    
    * test(template): stabilize virtual scrolling component tests for CI
    
    * chore: integrate component-test into ci
    
    * test(template): fix jest & cypress type conflict
    
    * chore: fix broken html-webpack-plugin dependency
    
    * chore: stick to node lts version in ci
    
    * chore: stick to latest working node version in ci
    
    * chore: properly execute component-test in ci
    
    * docs(template): polish virtual-scrolling docs
    
    Co-authored-by: Kirill <karnaukhov.kh@gmail.com>
    
    * refactor(template): rename Autosize ScrollStrategy to be consistent
    
    * docs(template): improve virtual-scrolling docs
    
    * test(template): fix autosize runway height test
    
    ---------
    
    Co-authored-by: Kirill <karnaukhov.kh@gmail.com>

---
+++ libs/template/experimental/virtual-scrolling/src/lib/index.ts
@@ -0,0 +1,16 @@
+export {
+  ListRange,
+  RxVirtualForViewContext,
+  RxVirtualScrollStrategy,
+  RxVirtualViewRepeater,
+} from './model';
+export * from './scroll-strategies/autosize-virtual-scroll-strategy';
+export * from './scroll-strategies/dynamic-size-virtual-scroll-strategy';
+export * from './scroll-strategies/fixed-size-virtual-scroll-strategy';
+export * from './virtual-for.directive';
+export {
+  RX_VIRTUAL_SCROLL_DEFAULT_OPTIONS,
+  RX_VIRTUAL_SCROLL_DEFAULT_OPTIONS_FACTORY,
+  RxVirtualScrollDefaultOptions,
+} from './virtual-scroll.config';
+export * from './virtual-scroll-viewport.component';
---
+++ libs/template/experimental/virtual-scrolling/tests/fixtures.ts
@@ -0,0 +1,100 @@
+import { NgIterable } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { createOutputSpy, mount } from 'cypress/angular';
+import { Observable, Subject } from 'rxjs';
+import {
+  FixedSizeVirtualScrollStrategy,
+  ListRange,
+  RxVirtualFor,
+  RxVirtualScrollViewportComponent,
+} from '../src/index';
+import { RxStrategyNames } from '@rx-angular/cdk/render-strategies';
+
+import {
+  DEFAULT_ITEM_SIZE,
+  DEFAULT_RUNWAY_ITEMS,
+  DEFAULT_RUNWAY_ITEMS_OPPOSITE,
+  DEFAULT_VIEW_CACHE_SIZE,
+} from '../src/lib/virtual-scroll.config';
+
+function randomContent(minLength = 1, maxLength = 50) {
+  return new Array(Math.max(minLength, Math.floor(Math.random() * maxLength)))
+    .fill('')
+    .map(() => randomWord())
+    .join(' ');
+}
+
+function randomWord() {
+  const words = [
+    'Apple',
+    'Banana',
+    'The',
+    'Orange',
+    'House',
+    'Boat',
+    'Lake',
+    'Car',
+    'And',
+  ];
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+function randomDate() {
+  const start = new Date('2000-01-01');
+  const end = new Date();
+  const diff = (end.getTime() - start.getTime()) / (1000 * 60 * 60 * 24);
+  const randomDiff = Math.floor(Math.random() * diff);
+  const randomDate = new Date(
+    start.getTime() + randomDiff * (1000 * 60 * 60 * 24)
+  );
+  return randomDate;
+}
+
+export interface Item {
+  id: number;
+  value: string;
+  content: string;
+  date: Date;
+  description: string;
+}
+
+export function generateItems(amount: number, startId = 0): Item[] {
+  return new Array(amount).fill(0).map((v, i) => ({
+    id: startId + i,
+    value: `value ${startId + i}`,
+    content: randomContent(),
+    date: randomDate(),
+    description: Math.random() >= 0.5 ? randomContent(30, 50) : '',
+  }));
+}
+
+export interface VirtualScrollMountConfig<T> {
+  runwayItems?: number;
+  runwayItemsOpposite?: number;
+  viewCache?: number;
+  items?: Observable<NgIterable<T>> | NgIterable<T> | null | undefined;
+  trackBy?: keyof T | ((idx: number, i: T) => unknown);
+  itemSize?: number;
+  strategy?: RxStrategyNames<string> | Observable<RxStrategyNames<string>>;
+  containerHeight?: number;
+}
+
+export const defaultMountConfig: VirtualScrollMountConfig<Item> = {
+  runwayItems: DEFAULT_RUNWAY_ITEMS,
+  runwayItemsOpposite: DEFAULT_RUNWAY_ITEMS_OPPOSITE,
+  itemSize: DEFAULT_ITEM_SIZE,
+  viewCache: DEFAULT_VIEW_CACHE_SIZE,
+  containerHeight: 300,
+} as const;
+export const defaultItemLength = 500;
+export function getDefaultMountConfig(): VirtualScrollMountConfig<Item> {
+  return {
+    items: generateItems(defaultItemLength),
+    ...defaultMountConfig,
+  };
+}
+export function getViewportComponent(fixture) {
+  return fixture.debugElement.query(
+    By.directive(RxVirtualScrollViewportComponent)
+  ).componentInstance as RxVirtualScrollViewportComponent;
+}
