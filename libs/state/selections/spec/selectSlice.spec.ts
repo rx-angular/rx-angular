b340d99664e17448d7ae877f394e4a32d44ea18b
Edouard Bozon <bozonedouard@gmail.com>
2023-05-07 21:08:58 +0200

    chore: migrate to Nx v16

--- libs/state/selections/spec/selectSlice.spec.ts
+++ libs/state/selections/spec/selectSlice.spec.ts
@@ -1,10 +1,10 @@
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+// eslint-disable-next-line @nx/enforce-module-boundaries
 import { jestMatcher } from '@test-helpers';
 import { Observable, of } from 'rxjs';
 import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 import { map, mergeMap } from 'rxjs/operators';
 import { TestScheduler } from 'rxjs/testing';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+// eslint-disable-next-line @nx/enforce-module-boundaries
 import { KeyCompareMap, selectSlice } from '@rx-angular/state/selections';
 
 let testScheduler: TestScheduler;
