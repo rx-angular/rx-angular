b77befb94f1a76ad7a326ecb788ab6af563273d4
Michael <michael.hladky@push-based.io>
2023-06-27 10:40:35 +0200

    refactor: refactor test setup

--- libs/state/actions/src/lib/actions.spec.ts
+++ libs/state/actions/src/lib/actions.spec.ts
@@ -2,136 +2,72 @@
 import { mockConsole } from '@test-helpers';
 import { rxActions } from './actions';
 import { isObservable } from 'rxjs';
-import { Component, ErrorHandler } from '@angular/core';
+import { Component, ErrorHandler, Provider, Type } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { RxActions } from '@rx-angular/state/actions';
+import { ActionTransforms } from '@rx-angular/state/actions';
 
-type Actions = { prop: string; prop2: string; search: string; resize: number };
-
-let fixture = undefined;
-let errorfixture = undefined;
-let errorComponentInstance = undefined;
-let _actions: RxActions<Actions> = undefined;
-let _actions2: RxActions<Actions> = undefined;
-let _actionstransform: RxActions<Actions> = undefined;
-
-// tslint:disable-next-line: prefer-on-push-component-change-detection  use-component-selector
-@Component({
-  template: '',
-})
-class TestComponent {
-  ui = rxActions<Actions>();
-  ui2 = rxActions<Actions>();
-}
-
-@Component({
-  template: '',
-})
-class Test2Component {
-  ui = rxActions<Actions>(({ transforms }) => {
-    transforms({
-      prop: (x: any) => 'transformed',
-      search: (e: InputEvent | string): string => {
-        return typeof e === 'object' ? (e as any).target.value : e;
-      },
-      resize: (_: string | number): number => {
-        throw new Error('something went wrong');
-      },
-    });
-  });
-  ui2 = rxActions<Actions>();
-}
-
-const customErrorHandler: ErrorHandler = {
-  handleError: jest.fn(),
-};
-@Component({
-  template: '',
-  providers: [
-    {
-      provide: ErrorHandler,
-      useValue: customErrorHandler,
-    },
-  ],
-})
-class TestErrorComponent {
-  ui = rxActions<Actions>(({ transforms }) => {
-    transforms({
-      resize: (_: string | number): number => {
-        throw new Error('something went wrong');
-      },
-    });
-  });
-}
-
-/** @test {RxActionFactory} */
 describe('actions fn', () => {
   beforeAll(() => mockConsole());
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TestComponent],
-      providers: [],
-    }).compileComponents();
-    fixture = TestBed.createComponent(TestComponent);
-    errorfixture = TestBed.createComponent(TestErrorComponent);
-    errorComponentInstance = fixture.componentInstance;
-    _actionstransform =
-      TestBed.createComponent(Test2Component).componentInstance.ui;
-    _actions = fixture.componentInstance.ui;
-    _actions2 = fixture.componentInstance.ui2;
-  });
-
   it('should get created properly', () => {
-    expect(typeof _actions.prop).toBe('function');
-    expect(isObservable(_actions.prop)).toBeFalsy();
-    expect(isObservable(_actions.prop$)).toBeTruthy();
+    const { component } = setupComponent<Actions>();
+    expect(typeof component.actions.prop).toBe('function');
+    expect(isObservable(component.actions.prop)).toBeFalsy();
+    expect(isObservable(component.actions.prop$)).toBeTruthy();
   });
 
   it('should emit on the subscribed channels', (done) => {
     const values = 'foo';
     const exp = values;
-    _actions.prop$.subscribe((result) => {
+    const { component } = setupComponent<Actions>();
+    component.actions.prop$.subscribe((result) => {
       expect(result).toBe(exp);
       done();
     });
-    _actions.prop(values);
+    component.actions.prop(values);
   });
 
   it('should maintain channels per create call', (done) => {
     const values = 'foo';
     const nextSpy = jest.spyOn({ nextSpy: (_: string) => void 0 }, 'nextSpy');
     const exp = values;
-
-    _actions2.prop$.subscribe(nextSpy as unknown as (_: string) => void);
-    _actions.prop$.subscribe((result) => {
+    const { component } = setupComponent<Actions>();
+    component.actions2.prop$.subscribe(
+      nextSpy as unknown as (_: string) => void
+    );
+    component.actions.prop$.subscribe((result) => {
       expect(result).toBe(exp);
       done();
     });
     expect(nextSpy).not.toHaveBeenCalled();
-    _actions.prop(values);
+    component.actions.prop(values);
   });
 
   it('should emit and transform on the subscribed channels', (done) => {
     const exp = 'transformed';
-    _actionstransform.prop$.subscribe((result) => {
+    const { component } = setupComponent<Actions>({
+      transformFns: { prop: (x: any) => 'transformed' },
+    });
+    component.actions.prop$.subscribe((result) => {
       expect(result).toBe(exp);
       done();
     });
-    _actionstransform.prop('');
+    component.actions.prop('');
   });
 
   it('should emit on multiple subscribed channels', (done) => {
     const value1 = 'foo';
     const value2 = 'bar';
     const res = {};
-    _actions.prop$.subscribe((result) => {
+    const { component } = setupComponent<Actions>();
+
+    component.actions.prop$.subscribe((result) => {
       res['prop'] = result;
     });
-    _actions.prop2$.subscribe((result) => {
+    component.actions.prop2$.subscribe((result) => {
       res['prop2'] = result;
     });
-    _actions({ prop: value1, prop2: value2 });
+    component.actions({ prop: value1, prop2: value2 });
     expect(res).toStrictEqual({ prop: value1, prop2: value2 });
     done();
   });
@@ -141,11 +77,13 @@ describe('actions fn', () => {
     const value2 = 'bar';
 
     const res = [];
-    expect(typeof _actions.$).toBe('function');
-    _actions.$(['prop', 'prop2']).subscribe((result) => {
+    const { component } = setupComponent<Actions>();
+
+    expect(typeof component.actions.$).toBe('function');
+    component.actions.$(['prop', 'prop2']).subscribe((result) => {
       res.push(result);
     });
-    _actions({ prop: value1, prop2: value2 });
+    component.actions({ prop: value1, prop2: value2 });
     expect(res.length).toBe(2);
     expect(res).toStrictEqual([value1, value2]);
     done();
@@ -155,40 +93,91 @@ describe('actions fn', () => {
     let numCalls = 0;
     let numCalls2 = 0;
 
-    const factory = fixture.componentInstance.actions;
+    const { component, fixture } = setupComponent<Actions>();
 
-    _actions.prop$.subscribe(() => ++numCalls);
-    _actions2.prop$.subscribe(() => ++numCalls2);
+    component.actions.prop$.subscribe(() => ++numCalls);
+    component.actions2.prop$.subscribe(() => ++numCalls2);
     expect(numCalls).toBe(0);
     expect(numCalls2).toBe(0);
-    _actions.prop('');
-    _actions2.prop('');
+    component.actions.prop('');
+    component.actions2.prop('');
     expect(numCalls).toBe(1);
     expect(numCalls2).toBe(1);
     fixture.destroy();
-    _actions.prop('');
-    _actions2.prop('');
+    component.actions.prop('');
+    component.actions2.prop('');
     expect(numCalls).toBe(1);
     expect(numCalls2).toBe(1);
     done();
   });
 
   it('should throw if a setter is used', (done) => {
+    const { component } = setupComponent<Actions>();
     expect(() => {
-      (_actions as any).prop = 0;
+      (component.actions as any).prop = 0;
     }).toThrow('');
 
     done();
   });
 
-  it('should isolate errors and invoke provided ErrorHandler', async () => {
-    const fixture = TestBed.createComponent(TestErrorComponent);
-
-    fixture.componentInstance.ui.search('');
-    fixture.componentInstance.ui.resize(42);
+  it('should isolate errors and invoke provided ErrorHandler', () => {
+    const customErrorHandler: ErrorHandler = {
+      handleError: jest.fn(),
+    };
+    const { fixture } = setupComponent<Actions>({
+      transformFns: {
+        resize: (_: string | number): number => {
+          throw new Error('something went wrong');
+        },
+      },
+      providers: [
+        {
+          provide: ErrorHandler,
+          useValue: customErrorHandler,
+        },
+      ],
+    });
+    fixture.componentInstance.actions.search('');
+    fixture.componentInstance.actions.resize(42);
 
     expect(customErrorHandler.handleError).toHaveBeenCalledWith(
       new Error('something went wrong')
     );
   });
 });
+
+type Actions = { prop: string; prop2: string; search: string; resize: number };
+function setupComponent<
+  Actions extends object,
+  Transforms extends ActionTransforms<Actions> = {}
+>(cfg?: { transformFns?: Transforms; providers?: Provider[] }) {
+  let providers = [];
+  if (Array.isArray(cfg?.providers)) {
+    providers = cfg.providers;
+  }
+  @Component({
+    template: '',
+    providers,
+  })
+  class TestComponent {
+    actions = rxActions<Actions>(({ transforms }) => {
+      if (cfg?.transformFns) {
+        transforms(cfg.transformFns);
+      }
+    });
+    actions2 = rxActions<Actions>(({ transforms }) => {
+      if (cfg?.transformFns) {
+        transforms(cfg.transformFns);
+      }
+    });
+  }
+
+  TestBed.configureTestingModule({
+    declarations: [TestComponent],
+  });
+
+  const fixture = TestBed.createComponent(TestComponent);
+  const component = fixture.componentInstance;
+
+  return { component, fixture };
+}
