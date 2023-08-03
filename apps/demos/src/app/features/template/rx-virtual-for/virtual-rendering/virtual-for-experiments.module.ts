b15a31192103805e0868c0afed03c59f5e8fa693
Julian Jandl <julian@jandl.wien>
2023-08-03 22:14:48 +0200

    feat(demos): add demos for decoupled scroll elements

--- apps/demos/src/app/features/template/rx-virtual-for/virtual-rendering/virtual-for-experiments.module.ts
+++ apps/demos/src/app/features/template/rx-virtual-for/virtual-rendering/virtual-for-experiments.module.ts
@@ -12,20 +12,32 @@ import {
   FixedSizeVirtualScrollStrategy,
   RxVirtualScrollViewportComponent,
   RxVirtualFor,
+  RxVirtualScrollWindowDirective,
+  RxVirtualScrollElementDirective,
 } from '@rx-angular/template/experimental/virtual-scrolling';
 import { RxFor } from '@rx-angular/template/for';
 import { RxIf } from '@rx-angular/template/if';
 import { RxLet } from '@rx-angular/template/let';
 import { StrategySelectModule } from '../../../../shared/debug-helper/strategy-select/index';
 import { ValueProvidersModule } from '../../../../shared/debug-helper/value-provider/index';
-import { VirtualForTestComponent } from './virtual-for-test.component';
+import { VirtualForDemoComponent } from './virtual-for-demo.component';
+import { VirtualForScrollWindowDemoComponent } from './virtual-for-scroll-window-demo.component';
+import { VirtualForCustomScrollableDemoComponent } from './virtual-for-scrollable-demo.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       {
         path: '',
-        component: VirtualForTestComponent,
+        component: VirtualForDemoComponent,
+      },
+      {
+        path: 'custom-scroll',
+        component: VirtualForCustomScrollableDemoComponent,
+      },
+      {
+        path: 'window-scrolling',
+        component: VirtualForScrollWindowDemoComponent,
       },
     ]),
     ValueProvidersModule,
@@ -44,8 +56,14 @@ import { VirtualForTestComponent } from './virtual-for-test.component';
     RxIf,
     RxVirtualScrollViewportComponent,
     RxFor,
+    RxVirtualScrollWindowDirective,
+    RxVirtualScrollElementDirective,
   ],
-  declarations: [VirtualForTestComponent],
+  declarations: [
+    VirtualForDemoComponent,
+    VirtualForCustomScrollableDemoComponent,
+    VirtualForScrollWindowDemoComponent,
+  ],
   providers: [],
 })
 export class RxVirtualForModule {}
