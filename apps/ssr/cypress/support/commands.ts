1cd31dbf63961df4c0cd91d00e90d167406a0a34
Edouard Bozon <bozonedouard@gmail.com>
2023-10-27 15:26:15 +0200

    chore: move e2e in ssr app

--- apps/ssr/cypress/support/commands.ts
+++ apps/ssr/cypress/support/commands.ts
@@ -1,5 +1,7 @@
+/// <reference types="cypress" />
+
 // ***********************************************
-// This example commands.js shows you how to
+// This example commands.ts shows you how to
 // create various custom commands and overwrite
 // existing commands.
 //
@@ -8,8 +10,8 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 
-// eslint-disable-next-line @typescript-eslint/no-namespace
+// eslint-disable-next-line @typescript-eslint/no-namespace, @typescript-eslint/no-unused-vars
 declare namespace Cypress {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-interface
   interface Chainable<Subject> {}
 }
