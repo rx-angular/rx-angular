8822e269936f69a031ab2615cb3989ff7ccc7b70
Edouard Bozon <bozonedouard@gmail.com>
2024-01-25 16:05:09 +0100

    test(isr): add basic e2e tests

---
+++ apps/ssr-isr/cypress/support/commands.ts
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+// ***********************************************
+// This example commands.ts shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+
+// eslint-disable-next-line @typescript-eslint/no-namespace, @typescript-eslint/no-unused-vars
+declare namespace Cypress {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-interface
+  interface Chainable<Subject> {}
+}
