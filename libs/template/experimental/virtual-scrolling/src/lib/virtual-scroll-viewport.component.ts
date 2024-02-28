d71d1ecaa178d93e734db7a0039b2392fb8371ac
Julian Jandl <julian@jandl.wien>
2024-02-28 22:44:33 +0100

    chore: fix lint

--- libs/template/experimental/virtual-scrolling/src/lib/virtual-scroll-viewport.component.ts
+++ libs/template/experimental/virtual-scrolling/src/lib/virtual-scroll-viewport.component.ts
@@ -15,7 +15,6 @@ import {
 } from '@angular/core';
 import { defer, ReplaySubject, Subject } from 'rxjs';
 import { distinctUntilChanged, filter, take, takeUntil } from 'rxjs/operators';
-
 import {
   RxVirtualScrollElement,
   RxVirtualScrollStrategy,
