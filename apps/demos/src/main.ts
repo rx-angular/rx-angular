64b1271f1128b03a3d1c3365c31c16d710517191
Julian Jandl <julian@jandl.wien>
2024-04-04 23:01:53 +0200

    feat(demos): remove zone.js, implement signal query example

--- apps/demos/src/main.ts
+++ apps/demos/src/main.ts
@@ -1,18 +1,31 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { provideHttpClient } from '@angular/common/http';
+import { enableProdMode, ɵprovideZonelessChangeDetection } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { ROUTES } from './app/app-component/app.routes';
+import { AppComponent } from './app/app-component/index';
 
-import { AppModule } from './app/app.module';
+import { ENVIRONMENT_SETTINGS } from './app/shared/environment.token';
 import { environment } from './environments/environment';
 import { promiseMarkerFactory } from './app/shared/utils/measure';
 
-// if (environment.production) {
+if (environment.production) {
   enableProdMode();
-// }
+}
 
-const compilerOptions = environment.zoneless
-  ? ({ ngZone: 'noop' } as any)
-  : undefined;
 const mP = promiseMarkerFactory('Bootstrap');
 mP.wrap(
-  platformBrowserDynamic().bootstrapModule(AppModule)
+  bootstrapApplication(AppComponent, {
+    providers: [
+      provideHttpClient(),
+      provideAnimations(),
+      {
+        provide: ENVIRONMENT_SETTINGS,
+        useValue: environment,
+      },
+      ɵprovideZonelessChangeDetection(),
+      provideRouter(ROUTES),
+    ],
+  }),
 ).catch((err) => console.error(err));
