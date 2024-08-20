6dc9756f1f41d62169d6762cc379cbb04acbc465
Sam Lin <456807+maxisam@users.noreply.github.com.>
2024-08-20 10:39:24 -0500

    chore(isr): refactor IsrService patchState method

--- libs/isr/browser/src/isr.service.ts
+++ libs/isr/browser/src/isr.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { isPlatformServer } from '@angular/common';
 import { inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { IsrServiceInterface, IsrState } from '@rx-angular/isr/models';
@@ -10,7 +11,7 @@ export class IsrService implements IsrServiceInterface {
     if (isPlatformServer(this.platformId)) {
       throw new Error(
         `IsrService should not be instantiated on the server.
-          It is only used on the client as a injection token.`
+          It is only used on the client as a injection token.`,
       );
     }
   }
@@ -19,15 +20,23 @@ export class IsrService implements IsrServiceInterface {
     return { revalidate: null, errors: [], extra: {} };
   }
 
-  patchState(partialState: Partial<IsrState>): void {}
+  patchState(partialState: Partial<IsrState>): void {
+    return;
+  }
 
-  getExtra(): Record<string, any> {
+  getExtra(): Record<string, unknown> {
     return {};
   }
 
-  activate(): void {}
+  activate(): void {
+    return;
+  }
 
-  addError(error: Error): void {}
+  addError(error: Error): void {
+    return;
+  }
 
-  addExtra(extra?: Record<string, any> | undefined): void {}
+  addExtra(extra?: Record<string, unknown> | undefined): void {
+    return;
+  }
 }
