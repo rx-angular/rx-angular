ba1cbbefb4e16a220c263fdccbffcdb4d1a7ea37
Sam Lin <456807+maxisam@users.noreply.github.com.>
2024-08-20 14:47:59 -0500

    chore(isr): update CacheHandler and IsrServiceInterface typings

--- libs/isr/models/src/isr-service.interface.ts
+++ libs/isr/models/src/isr-service.interface.ts
@@ -1,14 +1,14 @@
 export interface IsrState {
   revalidate: number | null;
   errors: Error[];
-  extra: Record<string, any>;
+  extra: Record<string, unknown>;
 }
 
 export interface IsrServiceInterface {
   getState(): IsrState;
   patchState(partialState: Partial<IsrState>): void;
-  getExtra(): Record<string, any>;
+  getExtra(): Record<string, unknown>;
   activate(): void;
   addError(error: Error): void;
-  addExtra(extra?: Record<string, any>): void;
+  addExtra(extra?: Record<string, unknown>): void;
 }
