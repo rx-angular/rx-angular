0c66adefc33c4751b04dc1179c7b801a4065acd6
Julian Jandl <julian@jandl.wien>
2025-05-28 00:18:51 +0200

    chore: upgr nx & ng versions

--- libs/template/cypress.config.ts
+++ libs/template/cypress.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
     defaultCommandTimeout: 10000,
     specPattern: ['**/*.cy.ts'],
     viewportHeight: 600,
+    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
+    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
+    injectDocumentDomain: true,
   },
   viewportHeight: 600,
 });
