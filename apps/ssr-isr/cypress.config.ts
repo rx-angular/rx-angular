7fbd75436fbcd54d4b736617a4fb904d14c4f166
Enea Jahollari <jahollarienea14@gmail.com>
2025-06-19 21:44:52 +0200

    chore(template): fix tests

--- apps/ssr-isr/cypress.config.ts
+++ apps/ssr-isr/cypress.config.ts
@@ -4,8 +4,5 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     ...nxE2EPreset(__filename, { cypressDir: 'cypress' }),
-    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
-    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
-    injectDocumentDomain: true,
   },
 });
