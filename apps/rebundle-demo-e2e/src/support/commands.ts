d7bd6425b62bd1cad2312db9bb151f64409d3c8b
Julian Jandl <julian@jandl.wien>
2026-07-03 03:08:10 +0200

    test(rebundle): adjust e2e test

--- apps/rebundle-demo-e2e/src/support/commands.ts
+++ apps/rebundle-demo-e2e/src/support/commands.ts
@@ -11,17 +11,13 @@
 // ***********************************************
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
-declare namespace Cypress {
+/*declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
-    login(email: string, password: string): void;
+    customCommand(param: string): void;
   }
-}
+}*/
 
-// -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
-});
 //
 // -- This is a child command --
 // Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
