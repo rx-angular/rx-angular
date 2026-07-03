473e12893a8ed6fdbe840ce90ba40b41d89bf549
Enea Jahollari <jahollarienea14@gmail.com>
2026-07-03 10:41:30 -0400

    fix(template): revert removing viewport-prio directive

---
+++ libs/template/experimental/viewport-prio/src/lib/viewport-prio.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ViewportPrioDirective } from './viewport-prio.directive';
+
+/** @deprecated use the standalone import, will be removed with v16 */
+@NgModule({
+  imports: [ViewportPrioDirective],
+  exports: [ViewportPrioDirective],
+})
+export class ViewportPrioModule {}
